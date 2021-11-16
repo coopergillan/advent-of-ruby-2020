f1814b1d66c738b5824ee9f6560a7c3895a77f17
Cooper Gillan <cooper.gillan@gmail.com>
2021-11-16 10:21:00 -0600

    Day 1 - Parts 1 and 2 - Ruby Advent of Code 2020 (#1)
    * Add Gemfile(s) and ruby-version file
    
    * Day 1 - specs and solution
    
    Include some specs and a class and method that, combined, will read in
    the file and give the correct answer.
    
    I know that there is plenty of room for improvement here, especially
    given how much syntactic sugar Ruby has, but this will do for now.
    
    * Day 1 Part 1 - Some refactoring
    
    Move finding the elements to a private method and "just" request the
    answer in the public methods. This makes the initializer cleaner.
    
    * Switch references to answer to mention part 1
    
    * Change source data file name
    
    * Remove incorrect attribute and simplify main program
    
    * Day 1 Part 2 - some refactoring and part 2 solution
    
    Parameterize some of the part 1 solution to make it useful for answering
    part 2.
    
    * DRY up from_file and correct order in arrays
    
    * PR feedback refactor - use simpler subject
    
    * Use test data for just one test to verify it unpacks data correctly
    * Use `subject` with a simpler array for instantiating and build out the
    test cases from there

---
+++ day1/lib/day1.rb
@@ -0,0 +1,58 @@
+class Day1
+  attr_accessor :expense_report
+
+  def initialize(expense_report)
+    @expense_report = expense_report
+  end
+
+  def self.from_file(filepath)
+    new(File.foreach(filepath).map { |line| line.chomp.to_i })
+  end
+
+  def answer_part1(desired_sum)
+    element1, element2 = find_sum_elements(desired_sum, @expense_report)
+    if [element1, element2].all?
+      puts "Found #{element1} and #{element2} that sum to #{element1 + element2}"
+      product = element1 * element2
+      puts "Product is #{product}"
+      product
+    end
+  end
+
+  def answer_part2(desired_sum)
+    @expense_report.each_with_index do |elem, idx|
+      next_desired_sum = desired_sum - elem
+      all_others = @expense_report[0...idx] + @expense_report[(idx + 1)..]
+      second, third = find_sum_elements(next_desired_sum, all_others)
+      if [second, third].all?
+        puts "Found #{elem}, #{second}, #{third} - sum is #{desired_sum}"
+        product = elem * second * third
+        puts "Product is #{product}"
+        return product
+      end
+    end
+  end
+
+  private
+
+  def find_sum_elements(desired_sum, array_to_search)
+    array_to_search.each_with_index do |line, idx|
+      desired_element = desired_sum - line.to_i
+      all_others = array_to_search[0...idx] + array_to_search[(idx + 1)..]
+      if all_others.include?(desired_element)
+        return line, desired_element
+      end
+    end
+    return
+  end
+end
+
+if $PROGRAM_NAME  == __FILE__
+  day1 = Day1.from_file("lib/day1_data.txt")
+
+  puts "Answering part 1"
+  day1.answer_part1(2020)
+
+  puts "Answering part 2"
+  day1.answer_part2(2020)
+end
---
+++ day1/spec/day1_spec.rb
@@ -0,0 +1,41 @@
+require "day1"
+
+describe Day1 do
+
+  it "converts a file path to an array" do
+    day1 = Day1.from_file("spec/test_input.txt")
+    expect(day1.expense_report).to match_array([21, 32, 10, 40, 55])
+  end
+
+  context "answer methods" do
+    subject { Day1.new([2, 3, 4, 5, 7]) }
+
+    context "#answer_part1" do
+      it "returns the product when two numbers equal the given sum" do
+        expect(subject.answer_part1(8)).to eq(3 * 5)
+      end
+
+      it "returns the product when repeating an element would equal same sum" do
+        expect(subject.answer_part1(10)).to eq(7 * 3)  # Instead of 5 repeating twice
+      end
+
+      it "returns nil when no combination can be found that equals the sum" do
+        expect(subject.answer_part1(200)).to be_nil
+      end
+    end
+
+    context "#answer_part2" do
+      it "returns the product when three numbers equal the given sum" do
+        expect(subject.answer_part2(16)).to eq(4 * 5 * 7)
+      end
+
+      it "returns the product when three numbers equal the given sum when two also could" do
+        expect(subject.answer_part2(9)).to eq(2 * 3 * 4)  # 4 and 5 would make nine, but three numbers are desired
+      end
+
+      it "returns nil when no combination equaling the given sum can be found" do
+        expect(subject.answer_part1(3141)).to be_nil
+      end
+    end
+  end
+end
