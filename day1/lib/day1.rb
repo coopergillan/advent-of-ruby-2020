fd3d9b20c467bade9027dfc9e996b373f66014d3
Cooper Gillan <cooper.gillan@gmail.com>
2021-11-16 04:11:49 +0000

    Day 1 Part 1 - Some refactoring
    Move finding the elements to a private method and "just" request the
    answer in the public methods. This makes the initializer cleaner.

--- day1/lib/day1.rb
+++ day1/lib/day1.rb
@@ -1,44 +1,47 @@
 class Day1
-  attr_accessor :expense_report, :desired_sum
+  attr_accessor :expense_report
 
-  def initialize(expense_report, desired_sum)
+  def initialize(expense_report)
     @expense_report = expense_report
-    @desired_sum = desired_sum
   end
 
-  def self.from_file(filepath, desired_sum)
+  def self.from_file(filepath)
     expense_report_data = []
     File.foreach(filepath).each do |line|
       expense_report_data.append(line.chomp.to_i)
     end
-    new(expense_report_data, desired_sum)
+    new(expense_report_data)
   end
 
-  def find_sum_elements
+  def calculate_answer(desired_sum)
+    element1, element2 = find_sum_elements(desired_sum)
+    if [element1, element2].all?
+      puts "Found #{element1} and #{element2} that sum to #{@desired_sum}"
+      element1 * element2
+    end
+  end
+
+  private
+
+  def find_sum_elements(desired_sum)
     @expense_report.each_with_index do |line, index|
-      desired_element = @desired_sum - line.to_i
+      desired_element = desired_sum - line.to_i
       all_others = @expense_report[(index + 1)..] + @expense_report[0...index]
       if all_others.include?(desired_element)
-        return [line, desired_element]
+        return line, desired_element
       end
     end
     return
   end
-
-  def calculate_answer
-    element1, element2 = find_sum_elements
-    puts "Found #{element1} and #{element2} that sum to #{@desired_sum}"
-    element1 * element2
-  end
 end
 
 def answer_part1
-  part1 = Day1.from_file("lib/day1_part1.txt", 2020)
-  answer = part1.calculate_answer
+  part1 = Day1.from_file("lib/day1_part1.txt")
+  answer = part1.calculate_answer(2020)
   puts "Got answer: #{answer}"
 end
 
 if $PROGRAM_NAME  == __FILE__
-	puts "Answering part 1"
-	answer_part1
+  puts "Answering part 1"
+  answer_part1
 end
--- day1/spec/day1_spec.rb
+++ day1/spec/day1_spec.rb
@@ -1,32 +1,29 @@
 require "day1"
 
 describe Day1 do
-  it "converts a file path to an array" do
-    day1 = Day1.from_file("spec/test_input.txt", 1)
-
-    expect(day1.expense_report).to match_array(
-      [21, 32, 10, 40, 55],
-    )
+  before(:each) do
+    @day1 = Day1.from_file("spec/test_input.txt")
   end
 
-  context "#find_sum" do
-    it "returns two numbers that equal given sum" do
-      day1 = Day1.from_file("spec/test_input.txt", 87)
-      elements = day1.find_sum_elements
-      expect(elements).to match_array([32, 55])
-    end
-
-    it "returns nil if no combo is found" do
-      day1 = Day1.from_file("spec/test_input.txt", 200)
-      elements = day1.find_sum_elements
-      expect(elements).to be_nil
-    end
+  it "converts a file path to an array" do
+    expect(@day1.expense_report).to match_array([21, 32, 10, 40, 55])
   end
 
   context "#calculate_answer" do
-    it "returns the product of two numbers whose sum matches desired sum" do
-      day1 = Day1.from_file("spec/test_input.txt", 42)
-      expect(day1.calculate_answer).to eq(320)
+    context "when two numbers equal the given sum" do
+      it "returns the product" do
+        expect(@day1.calculate_answer(87)).to eq(32 * 55)
+      end
+
+      it "returns the product when repeating an element would equal same sum" do
+        expect(@day1.calculate_answer(42)).to eq(320)
+      end
+    end
+
+    context "when no combination can be found that equals the sum" do
+      it "returns nil" do
+        expect(@day1.calculate_answer(200)).to be_nil
+      end
     end
   end
 end
