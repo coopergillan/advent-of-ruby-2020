78a3bddfbfe1ba8aada3b6f9a6582d3d25f09583
Cooper Gillan <cooper.gillan@gmail.com>
2021-11-18 16:33:36 +0000

    Day 2 Part 1 answer with specs
    * Split Part1 into its own class to contain logic for the entries
    * Create a `count_valid_entries` method for the main PasswordList class
    that totals up entries based on the logic for Part1 entries

--- day2/lib/day2.rb
+++ day2/lib/day2.rb
@@ -1,6 +1,6 @@
 module Day2
   class PasswordList
-    attr_accessor :password_list
+    attr_accessor :password_list, :entry_counts
 
     def initialize(password_list)
       @password_list = password_list
@@ -9,27 +9,33 @@ module Day2
     def self.from_file(filepath)
       new(File.foreach(filepath).map { |line| line.chomp })
     end
+
+    def count_valid_entries(entry_klass)
+      @password_list.map { |raw_entry| entry_klass.new(raw_entry).valid? }.count(true)
+    end
   end
 
-  class Entry
-    attr_accessor :min_chars, :max_chars, :char, :password
+  class Part1
+    class Entry
+      attr_reader :min_chars, :max_chars, :char, :password
 
-    def initialize(min_chars, max_chars, char, password)
-      @min_chars = min_chars
-      @max_chars = max_chars
-      @char = char
-      @password = password
+      def initialize(raw_entry)
+        char_limits, raw_char, @password = raw_entry.split(" ")
+        @min_chars, @max_chars = char_limits.split("-").map { |raw_char| raw_char.to_i }
+        @char = raw_char.split(":").first
+      end
+
+      def valid?
+        @password.count(@char) >= @min_chars && @password.count(@char) <= @max_chars
+      end
     end
   end
 end
 
 if $PROGRAM_NAME  == __FILE__
-  puts "hello world"
-  # day2 = Day2.from_file("lib/day2_data.txt")
+  day2 = Day2::PasswordList.from_file("lib/day2_data.txt")
 
-  # puts "Answering part 1"
-  # day2.answer_part1(2020)
-  #
-  # puts "Answering part 2"
-  # day2.answer_part2(2020)
+  puts "Answering part 1"
+  valid_entries = day2.count_valid_entries(Day2::Part1::Entry)
+  puts "Valid password entries for part 1: #{valid_entries}"
 end
--- day2/spec/day2_spec.rb
+++ day2/spec/day2_spec.rb
@@ -1,59 +1,55 @@
 require "day2"
 
 describe Day2 do
+  describe Day2::PasswordList do
+    subject { described_class.from_file("spec/day2_test_input.txt") }
 
-  describe "PasswordList" do
-    it "converts a file path to an array" do
-      day2 = Day2::PasswordList.from_file("spec/day2_test_input.txt")
-      expect(day2.password_list).to match_array([
-        "9-10 b: bbktbbbxhfbpb",
-        "2-10 x: xxnxxxwxxsx",
-        "5-7 w: ghwwdrr",
-        "4-6 z: nzzjzk",
-        "7-8 s: szsssswfs",
-      ])
+    context "#from_file" do
+      it "converts a file path to an array" do
+        expect(subject.password_list).to match_array([
+          "9-10 b: bbktbbbxhfbpb",
+          "2-10 x: xxnxxxwxxsx",
+          "5-7 w: ghwwdrr",
+          "4-6 z: nzzjzk",
+          "7-8 s: szsssswfs",
+        ])
+      end
+    end
+
+    context "#count_valid_entries" do
+      it "counts valid entries for given file using Part1 definition of Entry" do
+        expect(subject.count_valid_entries(Day2::Part1::Entry)).to eq(1)
+      end
     end
   end
 
-  describe Day2::Entry do
-    it "instantiates each attribute from raw string" do
-      subject.new("2-3 j: abcdefghijk")
+  describe Day2::Part1::Entry do
+    let(:valid_entry ) { "1-4 j: abcdefghijk" }
+    subject { Day2::Part1::Entry }
 
-      expect subject.min_chars.to eq(2)
-      expect subject.max_chars.to eq(3)
-      expect subject.char.to eq("j")
-      expect subject.password.to eq("abcdefghijk")
+    context "#new" do
+      it "instantiates each attribute from raw string" do
+        day2_valid = subject.new(valid_entry)
+
+        expect(day2_valid).to have_attributes(
+          min_chars: 1,
+          max_chars: 4,
+          char: "j",
+          password: "abcdefghijk",
+        )
+      end
+    end
+
+    context "checking for valid or invalid entries" do
+      let(:invalid_entry) { "2-3 j: abcdefghijk" }
+
+      it "identifies a valid password" do
+        expect(subject.new(valid_entry).valid?).to be true
+      end
+
+      it "identifies an invalid password" do
+        expect(subject.new(invalid_entry).valid?).to be false
+      end
     end
   end
-  # context "answer methods" do
-  #   subject { Day2.new([2, 3, 4, 5, 7]) }
-  #
-  #   context "#answer_part1" do
-  #     it "returns the product when two numbers equal the given sum" do
-  #       expect(subject.answer_part1(8)).to eq(3 * 5)
-  #     end
-  #
-  #     it "returns the product when repeating an element would equal same sum" do
-  #       expect(subject.answer_part1(10)).to eq(7 * 3)  # Instead of 5 repeating twice
-  #     end
-  #
-  #     it "returns nil when no combination can be found that equals the sum" do
-  #       expect(subject.answer_part1(200)).to be_nil
-  #     end
-  #   end
-  #
-  #   context "#answer_part2" do
-  #     it "returns the product when three numbers equal the given sum" do
-  #       expect(subject.answer_part2(16)).to eq(4 * 5 * 7)
-  #     end
-  #
-  #     it "returns the product when three numbers equal the given sum when two also could" do
-  #       expect(subject.answer_part2(9)).to eq(2 * 3 * 4)  # 4 and 5 would make nine, but three numbers are desired
-  #     end
-  #
-  #     it "returns nil when no combination equaling the given sum can be found" do
-  #       expect(subject.answer_part1(3141)).to be_nil
-  #     end
-  #   end
-  # end
 end
