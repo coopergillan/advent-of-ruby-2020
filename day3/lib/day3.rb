f0f13c18667094b08e6e570ed940eb88b36a85ae
Cooper Gillan <cooper.gillan@gmail.com>
2021-11-19 20:22:30 -0600

    Day 3 Part 1 - Slope and sleds (#4)
    * Add Day 3 part 1 instructions and data
    
    * Passing specs and correct answer
    
    * Day 3 Part 1 - Some cleanup and a solution
    
    * Rename method and use reduce
    
    * Inline assignment for check

---
+++ day3/lib/day3.rb
@@ -0,0 +1,44 @@
+module Day3
+  class Terrain
+    attr_accessor :detail, :width
+
+    def initialize(detail)
+      @detail = detail
+      @width = detail.first.size
+    end
+
+    def self.from_file(filepath)
+      new(File.foreach(filepath).map(&:chomp))
+    end
+  end
+
+  class Part1
+    class Journey
+      attr_accessor :run, :rise
+
+      def initialize(run, rise)
+        @run = run
+        @rise = rise
+      end
+
+      def count_trees(terrain)
+        horizontal_pos = 0
+        (@rise...terrain.detail.size).step(@rise).each.reduce(0) do |tree_count, vertical_pos|
+          if (horizontal_pos += @run) >= terrain.width
+            horizontal_pos -= terrain.width
+          end
+          tree_count += terrain.detail[vertical_pos][horizontal_pos] == "#" ? 1 : 0
+        end
+      end
+    end
+  end
+end
+
+
+if $PROGRAM_NAME  == __FILE__
+  day3 = Day3::Terrain.from_file("lib/day3_data.txt")
+
+  puts "Answering part 1"
+  part1_count = Day3::Part1::Journey.new(3, 1).count_trees(day3)
+  puts "Got #{part1_count} trees"
+end
---
+++ day3/spec/day3_spec.rb
@@ -0,0 +1,39 @@
+require "day3"
+
+describe Day3 do
+  describe Day3::Terrain do
+    subject { described_class.from_file("spec/day3_test_input_small.txt") }
+
+    context "#from_file" do
+      it "converts a file path to an array" do
+        expect(subject.detail).to match_array([
+          "..##.......",
+          "#...#...#..",
+          ".#....#..#.",
+          "..#.#...#.#",
+          ".#...##..#.",
+        ])
+      end
+    end
+  end
+
+  describe Day3::Part1::Journey do
+    subject { described_class.new(3, 1) }
+
+    it "counts trees with a given slope and terrain for an example terrain" do
+      terrain = Day3::Terrain.new([
+        "..##.......",
+        "#...#...#..",
+        ".#....#..#.",
+        "..#.#...#.#",
+        ".#...##..#.",
+        "..#.##.....",
+        ".#.#.#....#",
+        ".#........#",
+        "#.##...#...",
+        "#...##....#",
+      ])
+      expect(subject.count_trees(terrain)).to eq(6)
+    end
+  end
+end
