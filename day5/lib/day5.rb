7b3aabc241799633c3c2fe575f36469ca96c2202
Cooper Gillan <cooper.gillan@gmail.com>
2021-11-24 03:41:33 +0000

    WIP with one spec passing and another teed up

---
+++ day5/lib/day5.rb
@@ -0,0 +1,67 @@
+module Day5
+  class BoardingPassList
+    attr_accessor :passes
+
+    def initialize(passes)
+      @passes = passes
+    end
+
+    def self.from_file(filepath)
+      new(File.foreach(filepath).map(&:chomp))
+    end
+  end
+
+  class Part1
+    ROWS = (0...128)
+    COLUMNS = (0...8)
+
+    class BoardingPass
+      attr_accessor :row_data, :column_data
+
+      def initialize(binary_input)
+        matcher = binary_input.match(/(?<row_data>^[BF]{7})(?<column_data>[LR]{3})/)
+
+        @row_data = matcher[:row_data].to_s
+        @column_data = matcher[:column_data].to_s
+      end
+
+      def row
+        row_finder = ROWS.to_a
+        @row_data.each_char do |char|
+          new_size = row_finder.size / 2
+          if char == "F"
+            row_finder = row_finder.first(new_size)
+          elsif char == "B"
+            row_finder = row_finder.last(new_size)
+          end
+        end
+        row_finder.first
+      end
+
+      def column
+        "hello world"
+      end
+    end
+  end
+end
+
+
+if $PROGRAM_NAME  == __FILE__
+  puts "hello world"
+  # day5 = Day5::Terrain.from_file("lib/day5_data.txt")
+  #
+  # puts "Answering part 1"
+  # part1_count = Day5::Part1::Journey.new(3, 1).count_trees(day5)
+  # puts "Got #{part1_count} trees"
+  #
+  # puts "Answering part 2"
+  # slopes = [
+  #     [1, 1],
+  #     [3, 1],
+  #     [5, 1],
+  #     [7, 1],
+  #     [1, 2],
+  # ]
+  # part2_answer = Day5::Part2::JourneyLog.new(slopes, day5).answer_part2
+  # puts "Got #{part2_answer} for part 2 answer"
+end
---
+++ day5/spec/day5_spec.rb
@@ -0,0 +1,48 @@
+require "day5"
+
+describe Day5 do
+  describe Day5::BoardingPassList do
+    subject { described_class.from_file("spec/day5_test_input.txt") }
+
+    context "#from_file" do
+      it "reads the list of boarding passes from a raw file" do
+        expect(subject.passes).to match_array([
+          "FBFBFBFLLR",
+          "BBFBFFBLRR",
+          "BBBFFFFLLR",
+          "FBFFFFFLRL",
+          "BFBFFFFLRL",
+        ])
+      end
+    end
+
+    context "#answer_part1"
+  end
+
+  describe Day5::Part1::BoardingPass do
+    subject { described_class.new("BFFFBBFRRR") }
+
+    it "reads the row and column data correctly" do
+      expect(subject).to have_attributes(
+        row_data: "BFFFBBF",
+        column_data: "RRR",
+      )
+    end
+
+    it "identifies the row" do
+      expect(subject.row).to eq(70)
+    end
+
+    it "identifies the column" do
+      expect(subject.column).to eq(7)
+    end
+
+    # context "#read_pass" do
+    #   it "identifies the row and column of the seat" do
+    #     expect(described_class.read_pass).to include(
+    #       row: 10, column: 5,
+    #     )
+    #   end
+    # end
+  end
+end
