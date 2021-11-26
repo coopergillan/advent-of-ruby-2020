e4c5fdf004f8c7cf3d822ae0a1e0083426fed652
Cooper Gillan <cooper.gillan@gmail.com>
2021-11-26 15:33:33 -0600

    Day 4 Part 2 - could have done some before (#8)
    * Add part 2 instructions and basic instantiation
    
    * Add BirthYear class with specs
    
    * Add IssueYear attribute class
    
    * Add ExpirationYear also
    
    * Finish up the Height class
    
    * Use regex for matching the new line chars - more Rubonic?
    
    * Clean up Height#valid? method
    
    * Add HairColor class with specs
    
    * Make regex matches a little stricter
    
    * Add PassportId class with specs
    
    * Clean up several class definitions and update some logic
    
    * Add final answer for part 2 before cleanup
    
    * Use between? and remove comments

--- day4/lib/day4.rb
+++ day4/lib/day4.rb
@@ -1,22 +1,28 @@
 module Day4
   class PassportList
-    attr_accessor :raw_list
+    attr_accessor :entries
 
-    def initialize(raw_list)
-      @raw_list = raw_list
+    def initialize(entries)
+      @entries = entries
     end
 
     def self.from_file(filepath)
       new(
-        File.read(filepath, chomp: true).split("\n\n").map do |line|
-          line.chomp.gsub("\n", " ")
+        File.read(filepath, chomp: true).split(/\n\n/).map do |line|
+          line.chomp.gsub(/\n/, " ")
         end
       )
     end
 
-    def count_valid_passports
-      @raw_list.reduce(0) do |valid_entries, raw_entry|
+    def count_valid_passports_part1
+      @entries.reduce(0) do |valid_entries, raw_entry|
         valid_entries + (Part1::Passport.new(raw_entry).valid? ? 1 : 0)
+      end
+    end
+
+    def count_valid_passports_part2
+      @entries.reduce(0) do |valid_entries, raw_entry|
+        valid_entries + (Part2::Passport.from_raw(raw_entry).valid? ? 1 : 0)
       end
     end
   end
@@ -47,7 +53,148 @@ module Day4
       private
 
       def fields
-        @raw_entry.split(" ").map { |raw_field| raw_field.split(":").first }
+        @raw_entry.split(" ").map { |raw_field| raw_field.split(":") }.to_h
+      end
+    end
+  end
+
+  class Part2
+    class BirthYear
+      def initialize(year)
+        @year = year.to_i
+      end
+
+      def valid?
+        @year.between?(1920, 2002)
+      end
+    end
+
+    class IssueYear
+      def initialize(year)
+        @year = year.to_i
+      end
+
+      def valid?
+        @year.between?(2010, 2020)
+      end
+    end
+
+    class ExpirationYear
+      def initialize(year)
+        @year = year.to_i
+      end
+
+      def valid?
+        @year.between?(2020, 2030)
+      end
+    end
+
+    class Height
+      attr_accessor :height, :units
+
+      def initialize(raw_input)
+        if !raw_input.nil?
+          matcher = raw_input.match(/(?<height>^\d+)(?<units>(cm|in))/)
+          if matcher
+            @height = matcher[:height].to_s.to_i
+            @units = matcher[:units].to_s
+          end
+        else
+          @height = nil
+          @units = nil
+        end
+      end
+
+      def valid?
+        if [@units, @height].all?
+          if @units == "in"
+            min = 59
+            max = 76
+          elsif @units == "cm"
+            min = 150
+            max = 193
+          end
+          return @height.between?(min, max)
+        end
+        false
+      end
+    end
+
+    class HairColor
+      attr_accessor :color
+
+      def initialize(color)
+        @color = color
+      end
+
+      def valid?
+        @color.match?(/^#[abcdef0-9]{6}$/)
+      end
+    end
+
+    class EyeColor
+      VALID_COLORS = %w{amb blu brn gry grn hzl oth}
+
+      attr_accessor :color
+
+      def initialize(color)
+        @color = color
+      end
+
+      def valid?
+        VALID_COLORS.include?(@color)
+      end
+    end
+
+    class PassportId
+      attr_accessor :id_number
+
+      def initialize(id_number)
+        @id_number = id_number.to_s
+      end
+
+      def valid?
+        @id_number.match?(/^\d{9}$/)
+      end
+    end
+
+    class Passport
+      REQUIRED_FIELDS = [
+        :byr,
+        :iyr,
+        :eyr,
+        :hgt,
+        :hcl,
+        :ecl,
+        :pid,
+      ]
+      ATTR_CLASSES = {
+        iyr: Part2::IssueYear,
+        byr: Part2::BirthYear,
+        eyr: Part2::ExpirationYear,
+        hgt: Part2::Height,
+        hcl: Part2::HairColor,
+        ecl: Part2::EyeColor,
+        pid: Part2::PassportId,
+      }
+
+      attr_accessor :attributes
+
+      def self.from_raw(raw_entry)
+        attributes = raw_entry.split(" ").map { |raw_field| raw_field.split(":") }.to_h { |k, v| [k.to_sym, v] }
+        new(attributes)
+      end
+
+      def initialize(attributes)
+        @attributes = attributes
+      end
+
+      def valid?
+        REQUIRED_FIELDS.map do |req_field|
+          if (input_value = @attributes[req_field])
+            ATTR_CLASSES[req_field].new(input_value).valid?
+          end
+        end.all?
       end
     end
   end
@@ -58,6 +205,10 @@ if $PROGRAM_NAME  == __FILE__
   raw_passport_list = Day4::PassportList.from_file("lib/day4_data.txt")
 
   puts "Answering part 1"
-  valid_passports = raw_passport_list.count_valid_passports
+  valid_passports = raw_passport_list.count_valid_passports_part1
+  puts "Got #{valid_passports} valid passports"
+
+  puts "Answering part 2"
+  valid_passports = raw_passport_list.count_valid_passports_part2
   puts "Got #{valid_passports} valid passports"
 end
--- day4/spec/day4_spec.rb
+++ day4/spec/day4_spec.rb
@@ -2,12 +2,13 @@ require "day4"
 
 describe Day4 do
   describe Day4::PassportList do
-    subject { described_class.from_file("spec/day4_test_input.txt") }
 
     context "#from_file" do
+      subject { described_class.from_file("spec/day4_part1_test_input.txt") }
+
       it "converts a file path to an array of raw passport strings" do
-        expect(subject.raw_list.size).to eq(4)
-        expect(subject.raw_list).to match_array([
+        expect(subject.entries.size).to eq(4)
+        expect(subject.entries).to match_array([
           "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm",
           "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929",
           "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm",
@@ -16,9 +17,51 @@ describe Day4 do
       end
     end
 
-    context "#count_valid_passports" do
+    context "#count_valid_passports_part1" do
       it "counts the valid passports" do
-        expect(subject.count_valid_passports).to eq(2)
+        valid1 = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
+        invalid_no_height = "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929"
+        valid2 = "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm"
+        invalid_no_byr = "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"
+
+        passport_list = described_class.new([
+          valid1, invalid_no_height, valid2, invalid_no_byr,
+        ])
+        expect(passport_list.count_valid_passports_part1).to eq(2)
+      end
+    end
+
+    context "#count_valid_passports_part2" do
+      let(:invalid_hgt) { "eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926" }
+      let(:invalid_no_hgt) { "iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946" }
+      let(:invalid_hcl) { "hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277" }
+      let(:invalid_several) { "hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007" }
+
+      let (:valid1) { "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f" }
+      let (:valid2) { "eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm" }
+      let (:valid3) { "hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022" }
+      let (:valid4) { "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719" }
+
+
+      it "finds no valid passports when only invalid entries given" do
+        passport_list = described_class.new([
+          invalid_hgt, invalid_no_hgt, invalid_hcl, invalid_several,
+        ])
+        expect(passport_list.count_valid_passports_part2.zero?).to be(true)
+      end
+
+      it "finds number of valid passports when valid entries given" do
+        passport_list = described_class.new([
+          valid1, valid2, valid3, valid4,
+        ])
+        expect(passport_list.count_valid_passports_part2).to eq(4)
+      end
+
+      it "finds number of valid passports when a mix of entries given" do
+        passport_list = described_class.new([
+          valid1, invalid_hgt, valid2, invalid_no_hgt, invalid_several, valid4,
+        ])
+        expect(passport_list.count_valid_passports_part2).to eq(3)
       end
     end
   end
@@ -43,6 +86,184 @@ describe Day4 do
         described_class.new(
           "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929",
         ).valid?).to be(false)
+    end
+  end
+
+  describe Day4::Part2::Passport do
+    context "#from_raw" do
+      it "instantiates all attributes with raw entry" do
+        raw_entry = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
+        passport = described_class.from_raw(raw_entry)
+        expect(passport.attributes).to include({
+          byr: "1937",
+          iyr: "2017",
+          eyr: "2020",
+          hgt: "183cm",
+          hcl: "#fffffd",
+          ecl: "gry",
+          pid: "860033327",
+        })
+      end
+    end
+  end
+
+  describe "Part 2 attribute classes" do
+    context Day4::Part2::BirthYear do
+      it "accepts a string and returns true for a BirthYear in the given range" do
+        birth_year = described_class.new("1937")
+        expect(birth_year.valid?).to be(true)
+      end
+
+      it "accepts a string and returns true for a BirthYear on the edge of the given range" do
+        birth_year = described_class.new("1920")
+        expect(birth_year.valid?).to be(true)
+      end
+
+      it "accepts a string and returns false for a BirthYear outside the given range" do
+        birth_year = described_class.new("2003")
+        expect(birth_year.valid?).to be(false)
+      end
+    end
+
+    context Day4::Part2::IssueYear do
+      it "accepts a string and returns true for an IssueYear in the given range" do
+        issue_year = described_class.new("2014")
+        expect(issue_year.valid?).to be(true)
+      end
+
+      it "accepts a string and returns true for an IssueYear on the edge of the given range" do
+        issue_year = described_class.new("2020")
+        expect(issue_year.valid?).to be(true)
+      end
+
+      it "accepts a string and returns false for an IssueYear outside the given range" do
+        issue_year = described_class.new("2009")
+        expect(issue_year.valid?).to be(false)
+      end
+    end
+
+    context Day4::Part2::ExpirationYear do
+      it "accepts a string and returns true for an ExpirationYear in the given range" do
+        expiration_year = described_class.new("2025")
+        expect(expiration_year.valid?).to be(true)
+      end
+
+      it "accepts a string and returns true for an ExpirationYear on the edge of the given range" do
+        expiration_year = described_class.new("2030")
+        expect(expiration_year.valid?).to be(true)
+      end
+
+      it "accepts a string and returns false for a ExpirationYear outside the given range" do
+        expiration_year = described_class.new("2019")
+        expect(expiration_year.valid?).to be(false)
+      end
+    end
+
+    context Day4::Part2::Height do
+      context "when height given in inches" do
+        it "returns true for a value within the range" do
+          height = described_class.new("62in")
+          expect(height.valid?).to be(true)
+        end
+
+        it "returns false for a value outside the range" do
+          height = described_class.new("80in")
+          expect(height.valid?).to be(false)
+        end
+
+        it "returns true for a value at the edge of the range" do
+          height = described_class.new("76in")
+          expect(height.valid?).to be(true)
+        end
+      end
+
+      context "when height given in centimeters" do
+        it "returns true for a value within the range" do
+          height = described_class.new("165cm")
+          expect(height.valid?).to be(true)
+        end
+
+        it "returns false for a value outside the range" do
+          height = described_class.new("140cm")
+          expect(height.valid?).to be(false)
+        end
+
+        it "returns true for a value at the edge of the range" do
+          height = described_class.new("193cm")
+          expect(height.valid?).to be(true)
+        end
+      end
+
+      context "when invalid input given" do
+        it "is invalid when other units are provided" do
+          height = described_class.new("2.4m")
+          expect(height.valid?).to be(false)
+        end
+        it "is invalid when no input provided" do
+          height = described_class.new(nil)
+          expect(height.valid?).to be(false)
+        end
+      end
+    end
+
+    context Day4::Part2::HairColor do
+      it "returns true for a valid hex color" do
+        hair_color = described_class.new("#602fce")
+        expect(hair_color.valid?).to be(true)
+      end
+
+      it "returns false for an invalid hex color" do
+        hair_color = described_class.new("#602qrs")
+        expect(hair_color.valid?).to be(false)
+      end
+
+      it "returns false for non-hex input" do
+        hair_color = described_class.new("Brown")
+        expect(hair_color.valid?).to be(false)
+      end
+    end
+
+    context Day4::Part2::EyeColor do
+      it "returns true for an accepted value" do
+        eye_color = described_class.new("gry")
+        expect(eye_color.valid?).to be(true)
+      end
+
+      it "returns false for a different value" do
+        eye_color = described_class.new("purple")
+        expect(eye_color.valid?).to be(false)
+      end
+    end
+
+    context Day4::Part2::PassportId do
+      context "given valid nine-digit input" do
+        it "returns true" do
+          passport_id = described_class.new("314159265")
+          expect(passport_id.valid?).to be(true)
+        end
+
+        it "returns true even when value starts with leading zeroes" do
+          passport_id = described_class.new("023332718")
+          expect(passport_id.valid?).to be(true)
+        end
+      end
+
+      context "given invalid input" do
+        it "returns false for not enough digits" do
+          passport_id = described_class.new("12345678")
+          expect(passport_id.valid?).to be(false)
+        end
+
+        it "returns false for too many digits" do
+          passport_id = described_class.new("1234567890")
+          expect(passport_id.valid?).to be(false)
+        end
+
+        it "returns false for non-digit input" do
+          passport_id = described_class.new("sweden")
+          expect(passport_id.valid?).to be(false)
+        end
+      end
     end
   end
 end
