4489f8fdc384148f2df0b10a6d50310b593428c4
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-01 09:06:59 -0600

    Day 5 part 1 - Boarding passes (#9)
    * Add instructions and puzzle input
    
    * WIP with one spec passing and another teed up
    
    * Add column finder
    
    * Answer for part 1 after some tweaking of specs, too
    
    Couldn't get max_by to work for the final answer portion, but got the
    right answer. Going to check it out later.

---
+++ day5/lib/day5.rb
@@ -0,0 +1,80 @@
+module Day5
+  class BoardingPassList
+    attr_accessor :passes
+
+    def initialize(passes)
+      @passes = passes
+    end
+
+    def self.from_file(filepath)
+      new(File.foreach(filepath).map(&:chomp))
+    end
+
+    def answer_part1
+      max_id = 0
+      @passes.each do |pass_input|
+        boarding_pass = Part1::BoardingPass.new(pass_input)
+        if boarding_pass.seat_id > max_id
+          max_id = boarding_pass.seat_id
+        end
+      end
+      max_id
+    end
+  end
+
+  class Part1
+    ROWS = (0...128)
+    COLUMNS = (0...8)
+
+    class BoardingPass
+      attr_accessor :row_data, :column_data
+
+      def initialize(binary_input)
+        matcher = binary_input.match(/(?<row_data>^[BF]{7})(?<column_data>[LR]{3})/)
+
+        @row_data = matcher[:row_data].to_s
+        @column_data = matcher[:column_data].to_s
+      end
+
+      def row
+        row_finder = ROWS.to_a
+        @row_data.each_char do |char|
+          new_size = row_finder.size / 2
+          if char == "F"
+            row_finder = row_finder.first(new_size)
+          elsif char == "B"
+            row_finder = row_finder.last(new_size)
+          end
+        end
+        row_finder.first
+      end
+
+      def column
+        col_finder = COLUMNS.to_a
+        @column_data.each_char do |char|
+          new_size = col_finder.size / 2
+          if char == "L"
+            col_finder = col_finder.first(new_size)
+          elsif char == "R"
+            col_finder = col_finder.last(new_size)
+          end
+        end
+        col_finder.first
+      end
+
+      def seat_id
+        (row * 8) + column
+      end
+    end
+  end
+end
+
+
+if $PROGRAM_NAME  == __FILE__
+  puts "hello world"
+  boarding_pass_list = Day5::BoardingPassList.from_file("lib/day5_data.txt")
+
+  puts "Answering part 1"
+  part1_answer = boarding_pass_list.answer_part1
+  puts "Max seat_id for the group is: #{part1_answer}"
+end
---
+++ day5/spec/day5_spec.rb
@@ -0,0 +1,47 @@
+require "day5"
+
+describe Day5 do
+  describe Day5::BoardingPassList do
+    subject { described_class.from_file("spec/day5_test_input.txt") }
+
+    context "#from_file" do
+      it "reads the list of boarding passes from a raw file" do
+        expect(subject.passes).to match_array([
+          "BFFFBBFRRR", # 567
+          "FFFBBBFRRR", # 119
+          "BBFFBBFRLL", # 820
+        ])
+      end
+    end
+
+    context "#answer_part1"
+    it "gets the highest seat_id for a list of boarding passes" do
+      expect(subject.answer_part1).to eq(820)
+    end
+  end
+
+  describe Day5::Part1::BoardingPass do
+    subject { described_class.new("BFFFBBFRRR") }
+
+    it "reads the row and column data correctly" do
+      expect(subject).to have_attributes(
+        row_data: "BFFFBBF",
+        column_data: "RRR",
+      )
+    end
+
+    it "identifies the row" do
+      expect(subject.row).to eq(70)
+    end
+
+    it "identifies the column" do
+      expect(subject.column).to eq(7)
+    end
+
+    context "#seat_id" do
+      it "gets the seat_id per instructions" do
+        expect(subject.seat_id).to eq(567)
+      end
+    end
+  end
+end
