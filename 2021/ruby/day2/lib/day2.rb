18131f8f3335869e0de793b751421790351e70a2
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-02 16:27:39 +0000

    Add part 2 solution and introduce Command class
    Add aim as an attribute and include separate navigation instructions for
    part 2. Also introduce a Command class to do the parsing of the "raw
    command" separately.
    
    More DRY-ing that could be done here, but this will do for now!

--- 2021/ruby/day2/lib/day2.rb
+++ 2021/ruby/day2/lib/day2.rb
@@ -1,10 +1,11 @@
 class Submarine
-  attr_accessor :instructions, :horizontal_position, :depth
+  attr_accessor :instructions, :horizontal_position, :depth, :aim
 
   def initialize(instructions)
     @instructions = instructions
     @horizontal_position = 0
     @depth = 0
+    @aim = 0
   end
 
   def self.from_file(filepath)
@@ -12,30 +13,63 @@ class Submarine
   end
 
   def answer_part1
-    navigate_all
+    navigate_all_part1
     @horizontal_position * @depth
   end
 
-  def navigate(command)
-    direction, raw_amount = command.split(/ /)
-    amount = raw_amount.to_i
-    case direction
-      when "forward" then @horizontal_position += amount
-      when "down" then @depth += amount
-      when "up" then @depth -= amount
+  def answer_part2
+    navigate_all_part2
+    @horizontal_position * @depth
+  end
+
+  def navigate_part1(raw_command)
+    command = Command.new(raw_command)
+    case command.direction
+    when "forward" then @horizontal_position += command.quantity
+      when "down" then @depth += command.quantity
+      when "up" then @depth -= command.quantity
+    end
+  end
+
+  def navigate_part2(raw_command)
+    command = Command.new(raw_command)
+    case command.direction
+      when "forward"
+        @horizontal_position += command.quantity
+        @depth += @aim * command.quantity
+      when "down" then @aim += command.quantity
+      when "up" then @aim -= command.quantity
     end
   end
 
   private
 
-  def navigate_all
-    @instructions.each { |command| navigate(command) }
+  def navigate_all_part1
+    @instructions.each { |raw_command| navigate_part1(raw_command) }
+  end
+
+  def navigate_all_part2
+    @instructions.each { |raw_command| navigate_part2(raw_command) }
+  end
+end
+
+class Command
+  attr_accessor :direction, :quantity
+
+  def initialize(raw_command)
+    @direction, raw_quantity = raw_command.split(/ /)
+    @quantity = raw_quantity.to_i
   end
 end
 
 if $PROGRAM_NAME  == __FILE__
-  submarine = Submarine.from_file("lib/input.txt")
+  part1_submarine = Submarine.from_file("lib/input.txt")
 
-  part1_location_product = submarine.answer_part1
+  part1_location_product = part1_submarine.answer_part1
   puts "Got #{part1_location_product} for part 1"
+
+  part2_submarine = Submarine.from_file("lib/input.txt")
+
+  part2_location_product = part2_submarine.answer_part2
+  puts "Got #{part2_location_product} for part 2"
 end
--- 2021/ruby/day2/spec/day2_spec.rb
+++ 2021/ruby/day2/spec/day2_spec.rb
@@ -15,27 +15,31 @@ describe Submarine do
       ])
       expect(subject.horizontal_position).to eq(0)
       expect(subject.depth).to eq(0)
+      expect(subject.aim).to eq(0)
     end
   end
 
-  context "#navigate" do
+  context "#navigate_part1" do
     it "increases horizontal position when given a forward command" do
-      subject.navigate("forward 7")
+      subject.navigate_part1("forward 7")
       expect(subject.horizontal_position).to eq(7)
       expect(subject.depth).to eq(0)
+      expect(subject.aim).to eq(0)
     end
 
     it "increases depth when given a down command" do
-      subject.navigate("down 6")
+      subject.navigate_part1("down 6")
       expect(subject.horizontal_position).to eq(0)
       expect(subject.depth).to eq(6)
+      expect(subject.aim).to eq(0)
     end
 
     it "decreases depth when given an up command" do
-      subject.navigate("down 6")
-      subject.navigate("up 2")
+      subject.navigate_part1("down 6")
+      subject.navigate_part1("up 2")
       expect(subject.horizontal_position).to eq(0)
       expect(subject.depth).to eq(4)
+      expect(subject.aim).to eq(0)
     end
   end
 
@@ -44,6 +48,77 @@ describe Submarine do
       expect(subject.answer_part1).to eq(15 * 10)
       expect(subject.horizontal_position).to eq(15)
       expect(subject.depth).to eq(10)
+    end
+  end
+
+  context "#navigate_part2" do
+    context "when given a down command" do
+      it "increases aim by the given amount" do
+        subject.navigate_part2("down 5")
+        expect(subject.aim).to eq(5)
+        expect(subject.horizontal_position).to eq(0)
+        expect(subject.depth).to eq(0)
+      end
+    end
+
+    context "when given an up command" do
+      it "decreases aim by the given amount" do
+        subject.navigate_part2("down 5")
+        subject.navigate_part2("up 2")
+        expect(subject.aim).to eq(3)
+        expect(subject.horizontal_position).to eq(0)
+        expect(subject.depth).to eq(0)
+      end
+    end
+
+    context "when given set of commands include forward" do
+      it "increases horizontal position and leaves aim unchanged at start" do
+        subject.navigate_part2("forward 3")
+        expect(subject.horizontal_position).to eq(3)
+        expect(subject.depth).to eq(0)
+        expect(subject.aim).to eq(0)
+      end
+
+      it "increases horizontal position and increases depth based on aim" do
+        subject.navigate_part2("forward 3")
+        subject.navigate_part2("down 5")
+        subject.navigate_part2("forward 6")
+        expect(subject.horizontal_position).to eq(9)
+        expect(subject.depth).to eq(30)
+        expect(subject.aim).to eq(5)
+      end
+
+      it "updates position, depth, and aim, according to the part 2 rules" do
+        subject.navigate_part2("forward 5")
+        expect(subject.horizontal_position).to eq(5)
+        expect(subject.depth).to eq(0)
+        expect(subject.aim).to eq(0)
+
+        subject.navigate_part2("down 5")
+        expect(subject.horizontal_position).to eq(5)
+        expect(subject.depth).to eq(0)
+        expect(subject.aim).to eq(5)
+
+        subject.navigate_part2("forward 8")
+        expect(subject.horizontal_position).to eq(13)
+        expect(subject.depth).to eq(40)
+        expect(subject.aim).to eq(5)
+
+        subject.navigate_part2("up 3")
+        expect(subject.horizontal_position).to eq(13)
+        expect(subject.depth).to eq(40)
+        expect(subject.aim).to eq(2)
+      end
+    end
+
+  end
+
+  context "#answer_part2" do
+    it "gets the product of horizontal position and depth" do
+      expect(subject.answer_part2).to eq(15 * 60)
+      expect(subject.horizontal_position).to eq(15)
+      expect(subject.depth).to eq(60)
+      expect(subject.aim).to eq(10)
     end
   end
 end
