520faf4910bb8a843df99642cfe23bb75953b5e7
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-04 21:10:15 +0000

    Add input and BingoGame class with specs

--- 2021/ruby/day4/lib/day4.rb
+++ 2021/ruby/day4/lib/day4.rb
@@ -1,13 +1,75 @@
-class Day4
-  attr_accessor :input
+class BingoGame
+  attr_accessor :drawn_numbers, :boards
 
-  def initialize(input)
-    @input = input
+  def initialize(drawn_numbers, boards)
+    @drawn_numbers = raw_input.split(/\n\n/).first
+    @boards = boards
   end
 
   def self.from_file(filepath)
-    new(File.foreach(filepath, chomp: true))
+    File.readlines(filepath, chomp: true).each_with_index do |line, idx|
+      if idx.zero?
+        drawn_numbers = line
+        puts "drawn_numbers:  #{drawn_numbers}"
+        next
+      end
+      puts "line: #{line}"
+    end
   end
+end
+
+class BingoBoard
+  attr_accessor :board
+
+  def initialize(board)
+    @board = board
+    @called_numbers = []
+  end
+
+  def self.from_raw(raw_board)
+    new(
+      raw_board.split(/\n/).map do |line|
+        line.split.map { |raw_number| {value: raw_number.to_i, called: false} }
+      end
+    )
+  end
+
+  def call_number(number)
+    @board.each do |row|
+      row.each do |spot|
+        if number == spot[:value]
+          spot[:called] = true
+        end
+      end
+    end
+  end
+
+  def uncalled_numbers
+    uncalled = []
+    @board.each do |row|
+      row.each do |spot|
+        if spot[:called] == false
+          uncalled.push(spot[:value])
+        end
+      end
+    end
+    uncalled
+  end
+
+  def has_win?
+    has_row_win? || has_column_win?
+  end
+
+  private
+
+  def has_row_win?
+    @board.map { |row| row.map { |spot| spot[:called] }.all? }.any?
+  end
+
+  def has_column_win?
+    @board.transpose.map { |row| row.map { |spot| spot[:called] }.all? }.any?
+  end
+
 end
 
 
--- 2021/ruby/day4/spec/day4_spec.rb
+++ 2021/ruby/day4/spec/day4_spec.rb
@@ -1,17 +1,141 @@
 require "day4"
 
-describe Day4 do
+describe BingoGame do
   subject { described_class.from_file("spec/test_input.txt") }
 
   context "#from_file" do
     it "converts a file path to an array" do
-      expect(subject.input).to match_array([
-        "what will it be?",
-      ])
+      expect(subject.drawn_numbers).to match_array(
+        %w{7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1}
+      )
     end
   end
 
   context "answering part 1" do
     it "hopefully does something soon"
+  end
+end
+
+describe BingoBoard do
+  let(:raw_board) { <<~BINGO
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+                    BINGO
+  }
+
+  subject { described_class.from_raw(raw_board) }
+
+  context "#new" do
+    it "reads in raw strings and converts them to 2x2 array of dicts with value and called boolean" do
+      expect(subject.board).to match_array([
+        [
+          {value: 22, called: false},
+          {value: 13, called: false},
+          {value: 17, called: false},
+          {value: 11, called: false},
+          {value:  0, called: false},
+        ],
+        [
+          {value:  8, called: false},
+          {value:  2, called: false},
+          {value: 23, called: false},
+          {value:  4, called: false},
+          {value: 24, called: false},
+        ],
+        [
+          {value: 21, called: false},
+          {value:  9, called: false},
+          {value: 14, called: false},
+          {value: 16, called: false},
+          {value:  7, called: false},
+        ],
+        [
+          {value:  6, called: false},
+          {value: 10, called: false},
+          {value:  3, called: false},
+          {value: 18, called: false},
+          {value:  5, called: false},
+        ],
+        [
+          {value:  1, called: false},
+          {value: 12, called: false},
+          {value: 20, called: false},
+          {value: 15, called: false},
+          {value: 19, called: false},
+        ],
+      ])
+    end
+  end
+
+  context "#call_number" do
+    it "update the called boolean to true when a given number matches a board" do
+      [22, 11, 12, 20, 15].each do |number|
+        subject.call_number(number)
+      end
+      expect(subject.board.first).to match_array([
+          {value: 22, called: true},
+          {value: 13, called: false},
+          {value: 17, called: false},
+          {value: 11, called: true},
+          {value:  0, called: false},
+        ])
+      expect(subject.board.last).to match_array([
+          {value:  1, called: false},
+          {value: 12, called: true},
+          {value: 20, called: true},
+          {value: 15, called: true},
+          {value: 19, called: false},
+      ])
+    end
+  end
+
+  context "#uncalled_numbers" do
+    context "when nthing has been called" do
+      it "returns all numbers" do
+        expect(subject.uncalled_numbers).to match_array([
+          22, 13,  17, 11,  0,
+           8,  2,  23,  4, 24,
+          21,  9,  14, 16,  7,
+           6, 10,  3, 18,  5,
+           1, 12, 20, 15, 19,
+        ])
+      end
+    end
+
+    context "when some numbers have been called" do
+      it "returns only the uncalled numbers" do
+        [13, 17, 0, 8,  24, 21,  9, 14, 6, 10, 1, 12, 20, 15].each do |number|
+          subject.call_number(number)
+        end
+        expect(subject.uncalled_numbers).to match_array(
+          [22, 11, 2, 23, 4, 16, 7, 3, 18, 5, 19],
+        )
+      end
+    end
+  end
+
+  context "#check_for_win" do
+    it "returns false if nothing has been called" do
+      expect(subject.has_win?).to be(false)
+    end
+
+    it "returns true if a row matches" do
+      first_row = [22, 13, 17, 11, 0]
+      first_row.each do |number|
+        subject.call_number(number)
+      end
+      expect(subject.has_win?).to be(true)
+    end
+
+    it "returns true if a column matches" do
+      second_column = [13, 2, 9, 10, 12]
+      second_column.each do |number|
+        subject.call_number(number)
+      end
+      expect(subject.has_win?).to be(true)
+    end
   end
 end
