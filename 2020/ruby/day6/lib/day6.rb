c5ac7c956de350307f8e5741b326914b64e4607a
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-04 05:33:11 +0000

    wip

--- 2020/ruby/day6/lib/day6.rb
+++ 2020/ruby/day6/lib/day6.rb
@@ -1,16 +1,18 @@
 class QuestionLog
-  attr_accessor :questions
+  attr_accessor :question_groups
 
-  def initialize(questions)
-    @questions = questions
+  def initialize(question_groups)
+    @question_groups = question_groups
   end
 
   def self.from_file(filepath)
     new(
       File.read(filepath, chomp: true).split(/\n\n/).map do |line|
-        line.chomp.gsub(/\n/, " ")
+        # line.chomp.gsub(/\n/, " ")
+        puts "line: #{line}"
+        line.chomp.split(/\n/).flatten
       end
-    )
+     )
   end
 end
 
--- 2020/ruby/day6/spec/day6_spec.rb
+++ 2020/ruby/day6/spec/day6_spec.rb
@@ -4,7 +4,12 @@ describe QuestionLog do
   context "#from_file" do
     subject { described_class.from_file("spec/test_input.txt") }
 
-    it "splits the input by blank lines for processing"
+    it "splits the input by blank lines for processing" do
+      expect(subject.question_groups).to match_array([
+
+      ])
+    end
+
 
   end
 
