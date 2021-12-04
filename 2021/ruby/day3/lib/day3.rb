9b8ef1ebfb5418870b560c2aad15e0dd88cfb004
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-03 20:30:43 -0600

    2021 - Ruby - Day 3 - Power consumption with some recursion (#14)
    * Day 3 Part 1 - A lot more than expected
    
    Came up with the idea for this only in my head and, not surprisingly, it
    was a little more challenging than expected to translate into code.
    
    I only wanted to have to loop through the whole file once so came up
    with the idea of storing the counts for each position on the way
    through. I can't quite tell if that introduced too much complexity, but
    at least I got the right answer.
    
    * A little part 1 cleanup before more part 2
    
    * Got Oxygen working with recursion
    
    * Part 2 answer with some recursion
    
    Add solution for part 2 by building two recursion functions that are
    nearly identical except for the equality check.
    
    * A little cleanup and refactoring
    
    * A little more cleanup - remove unnecessary attribute
    
    * One more cleanup

---
+++ 2021/ruby/day3/lib/day3.rb
@@ -0,0 +1,120 @@
+class DiagnosticReport
+  attr_accessor :bits
+
+  def initialize(bits)
+    @bits = bits.to_a
+  end
+
+  def self.from_file(filepath)
+    new(File.foreach(filepath, chomp: true))
+  end
+
+  def bit_counts(bits)
+    bit_counts_data = default_bit_counts(bits.first.size)
+    bits.each do |line|
+      line.each_char.to_a.each_with_index do |bit, idx|
+        bit_counts_data[idx.to_i] += bit.to_i
+      end
+    end
+    bit_counts_data
+  end
+
+  def most_common_bit_by_position(bits)
+    counts_hash = bit_counts(bits)
+    {}.tap do |hash|
+      counts_hash.each do |bit_position, count|
+        hash[bit_position] = (counts_hash[bit_position] >= (bits.size / 2.to_f)) ? 1 : 0
+      end
+    end
+  end
+
+  def gamma_rate_binary
+    most_common_bit_by_position(@bits).reduce("") do |binary_number, (_, most_common)|
+      binary_number += most_common.to_s
+    end
+  end
+
+  def gamma_rate
+    gamma_rate_binary.to_i(2)
+  end
+
+  def epsilon_rate_binary
+    most_common_bit_by_position(@bits).reduce("") do |binary_number, (_, most_common)|
+      binary_number += (most_common.zero? ? 1 : 0).to_s
+    end
+  end
+
+  def epsilon_rate
+    epsilon_rate_binary.to_i(2)
+  end
+
+  def part1
+    gamma_rate * epsilon_rate
+  end
+
+  def recursion_time(bits, start_position = 0, checking_o2: true)
+    most_common = most_common_bit_by_position(bits)
+    chars = []
+
+    bits.each do |bit|
+      bit_char = bit.chars[start_position].to_i
+      meets_o2_criteria = meets_o2_criteria?(bit_char, most_common[start_position])
+
+      if (checking_o2 && meets_o2_criteria) || (!checking_o2 && !meets_o2_criteria)
+        chars.push(bit)
+      end
+    end
+    return chars.first if chars.one?
+
+    start_position += 1
+    recursion_time(chars, start_position, checking_o2: checking_o2)
+  end
+
+  def oxygen_generator_rating_binary
+    recursion_time(@bits)
+  end
+
+  def oxygen_generator_rating
+    oxygen_generator_rating_binary.to_i(2)
+  end
+
+  def co2_scrubber_rating_binary
+    recursion_time(@bits, checking_o2: false)
+  end
+
+  def co2_scrubber_rating
+    co2_scrubber_rating_binary.to_i(2)
+  end
+
+  def life_support_rating
+    oxygen_generator_rating * co2_scrubber_rating
+  end
+
+  def part2
+    life_support_rating
+  end
+
+  private
+
+  def default_bit_counts(length)
+    {}.tap do |bit_counts_data|
+      (0...length).each do |i|
+        bit_counts_data[i] = 0
+      end
+    end
+  end
+
+  def meets_o2_criteria?(char, most_common_char)
+    return char == most_common_char
+  end
+end
+
+if $PROGRAM_NAME  == __FILE__
+  diagnostic_report = DiagnosticReport.from_file("lib/input.txt")
+
+  part1_power = diagnostic_report.part1
+  puts "Got #{part1_power} for part 1"
+
+  part2_power = diagnostic_report.part2
+  puts "Got #{part2_power} for part 2"
+end
---
+++ 2021/ruby/day3/spec/day3_spec.rb
@@ -0,0 +1,102 @@
+require "day3"
+
+describe DiagnosticReport do
+  subject { described_class.from_file("spec/test_input.txt") }
+  let(:bits) { [
+    "00100",
+    "11110",
+    "10110",
+    "10111",
+    "10101",
+    "01111",
+    "00111",
+    "11100",
+    "10000",
+    "11001",
+    "00010",
+    "01010",
+  ] }
+
+  context "#from_file" do
+    it "converts a file path to an array" do
+      expect(subject.bits.class).to eq(Array)
+      expect(subject.bits).to match_array(bits)
+    end
+  end
+
+  it "counts the occurrence of bits at each index" do
+    expect(subject.bit_counts(bits)).to include({
+      0 => 7,
+      1 => 5,
+      2 => 8,
+      3 => 7,
+      4 => 5,
+    })
+  end
+
+  it "reports the most common bit at each position" do
+    expect(subject.most_common_bit_by_position(bits)).to include({
+      0 => 1,
+      1 => 0,
+      2 => 1,
+      3 => 1,
+      4 => 0,
+    })
+  end
+
+  context "answering part1" do
+    it "gets the binary version of gamma rate using most_common_bit_by_position" do
+      expect(subject.gamma_rate_binary).to eq("10110")
+    end
+
+    it "gets the gamma rate by converting binary to decimal" do
+      expect(subject.gamma_rate).to eq(22)
+    end
+
+    it "gets the binary version of epsilon rate using most_common_bit_by_position" do
+      expect(subject.epsilon_rate_binary).to eq("01001")
+    end
+
+    it "gets the gamma rate by converting binary to decimal" do
+      expect(subject.epsilon_rate).to eq(9)
+    end
+
+    it "calculates the power consumption by multiplying the gamma and epsilon rates" do
+      expect(subject.part1).to eq(22 * 9) # 198
+    end
+  end
+
+  context "answering part2" do
+    it "runs a recursive check for to get the matching Oxygen generator rating binary" do
+      expect(subject.recursion_time(bits)).to eq("10111")
+    end
+
+    it "finds the oxygen generator rating in binary" do
+      expect(subject.oxygen_generator_rating_binary).to eq("10111")
+    end
+
+    it "finds the oxygen generator rating in decimal" do
+      expect(subject.oxygen_generator_rating).to eq(23)
+    end
+
+    it "uses a recursive check to get matching CO2 scrubber rating binary" do
+      expect(subject.recursion_time(bits, checking_o2: false)).to eq("01010")
+    end
+
+    it "finds CO2 scrubber rating in binary" do
+      expect(subject.co2_scrubber_rating_binary).to eq("01010")
+    end
+
+    it "finds CO2 scrubber rating in binary" do
+      expect(subject.co2_scrubber_rating).to eq(10)
+    end
+
+    it "calculates the life support rating by multiplying the oxygen generator rating by the CO2 scrubber rating" do
+      expect(subject.life_support_rating).to eq(23 * 10) # 230
+    end
+
+    it "gets the answer for part2" do
+      expect(subject.part2).to eq(230)
+    end
+  end
+end
