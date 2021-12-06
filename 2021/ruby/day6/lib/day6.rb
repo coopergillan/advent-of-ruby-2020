b491fd05da5e9c0eb179545f8089dba574e7f4a4
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-06 20:05:19 +0000

    Part 2 cleanup - rename and clean this up

--- 2021/ruby/day6/lib/day6.rb
+++ 2021/ruby/day6/lib/day6.rb
@@ -1,170 +1,54 @@
-class FishPopulation
-  attr_accessor :fish_population
+class School
+  attr_accessor :population
 
-  def initialize(fish_population)
-    @fish_population = population_to_hash(fish_population)
+  def initialize(population)
+    @population = population_to_hash(population)
   end
 
   def self.from_file(filepath)
     fish_array = File.read(filepath, chomp: true).split(",").map(&:to_i)
-    # fish_population = self.population_to_hash(fish_array)
     new(fish_array)
   end
 
-  def population_to_hash(fish_array)
-    fish_population = {}.tap do |hash|
-      (0..8).each do |i|
-        hash[i] = 0
-      end
-    end
-    fish_array.each { |fish| fish_population[fish] += 1 }
-    fish_population
-  end
-
   def simulate_day
-    start_population = @fish_population.clone
-    puts "start_population: #{start_population}"
-    @fish_population.size.times.reverse_each do |key|
-      puts "key: #{key}"
+    start_population = @population.clone
+
+    @population.size.times.reverse_each do |key|
       new_count_key = key - 1
-      if key == 0
-        # add the ones that spawned back up to 6
-        @fish_population[6] += start_population[key]
-        # Spawn the new ones
-        @fish_population[8] = start_population[key]
+      count_to_move = start_population[key]
+      if key.zero?
+        @population[6] += count_to_move
+        @population[8] = count_to_move
       else
-        puts "@fish_population[new_count_key]: #{@fish_population[new_count_key]}"
-        puts "start_population[key]: #{start_population[key]}"
-        @fish_population[new_count_key] = start_population[key]
-        puts "Current @fish_population: #{@fish_population}"
+        @population[new_count_key] = count_to_move
       end
     end
   end
 
   def part1(days = 80)
     days.times { simulate_day }
-
-    total_count = @fish_population.values.reduce(:+)
-    total_count
+    @population.values.reduce(:+)
   end
 
   def part2(days = 256)
     days.times { simulate_day }
-
-    total_count = @fish_population.values.reduce(:+)
-    total_count
+    @population.values.reduce(:+)
   end
 
-  # private
+  private
 
+  def population_to_hash(fish_array)
+    {}.tap { |hash| (0..8).each { |i| hash[i] = fish_array.count(i) } }
+  end
 end
-
-  # def simulate_day
-  #   new_fish_count = 0
-  #   @fish_population.each_with_index do |fish, idx|
-  #     # puts "Checking fish: #{fish} - which should match @fish_population[idx]: #{@fish_population[idx]}"
-  #     internal_timer = @fish_population[idx]
-  #     if internal_timer.zero?
-  #       new_fish_count +=1
-  #       internal_timer = 6
-  #     else
-  #       internal_timer -= 1
-  #     end
-  #     @fish_population[idx] = internal_timer
-  #     # puts "Finished checking fish: #{fish} - which should match @fish_population[idx]: #{@fish_population[idx]}"
-  #   end
-  #   if !new_fish_count.zero?
-  #     new_fish_count.times { @fish_population.push(8) }
-  #   end
-  # end
-
-  # def part1(days = 80)
-  #   days.times { simulate_day }
-  #   @fish_population.size
-  # end
-  #
-  # def part1_better_hopefully(days = 80)
-  #   simulate(@fish_population, days)
-  #   @fish_population.size
-  # end
-  #
-  # def part2(days = 256)
-  #   start_population = @fish_population
-  #   final = simulate(start_population, 256)
-  #   final.size
-  # end
-
-# def figure_it_out
-# # def figure_it_out(start_population)
-#   {
-#       0 => 0,
-#       1 => 0,
-#       2 => 0,
-#       3 => 0,
-#       4 => 0,
-#       5 => 0,
-#       6 => 0,
-#       7 => 0,
-#       8 => 0,
-#       }
-#   # {}.tap do |hash|
-#   #   8.times do |number|
-#   #     hash[number] = 0
-#   #   end
-#   # end
-#   # puts "hash: #{hash}"
-#   # hash
-# end
-#
-#
-# def simulate(population, days)
-#   return population if days.zero?
-#
-#   new_fish = 0
-#   puts "Checking day #{days}"
-#   # puts "population: #{population}"
-#   # puts "population.size: #{population.size}"
-#   population.each_with_index do |fish, idx|
-#     internal_timer = population[idx]
-#     if internal_timer.zero?
-#       new_fish += 1
-#       internal_timer = 6
-#     else
-#       internal_timer -= 1
-#     end
-#     population[idx] = internal_timer
-#   end
-#   if !new_fish.zero?
-#     new_fish.times { population << 8 }
-#   end
-#   # puts "Have #{days} befopre decrement"
-#   days -= 1
-#   # puts "Have #{days} after decrement"
-#   simulate(population, days)
-# end
-#
-# class School
-#   attr_accessor :population
-#
-#   def initialize(population)
-#     @population = population
-#   end
-#
-#   def simulate_day
-#     @population
-#   end
-# end
 
 
 if $PROGRAM_NAME  == __FILE__
-  # fish_population1 = FishPopulation.from_file("lib/input.txt")
-  # part1_answer = fish_population1.part1
-  # puts "Answer for part 1: #{part1_answer}"
+  school1 = School.from_file("lib/input.txt")
+  part1_answer = school1.part1
+  puts "Answer for part 1: #{part1_answer}"
 
-  fish_population2 = FishPopulation.from_file("lib/input.txt")
-  # fish_population2 = FishPopulation.new([
-# 1,1,1,1,2,1,1,4,1,4,3,1,1,1,1,1,1,1,1,4,1,3,1,1,1,5,1,3,1,4,1,2,1,1,5,1,1,1,1,1,1,1,1,1,1,3,4,1,5,1,1,1,1,1,1,1,1,1,3,1,4,1,1,1,1,3,5,1,1,2,1,1,1,1,4,4,1,1,1,4,1,1,4,2,4,4,5,1,1,1,1,2,3,1,1,4,1,5,1,1,1,3,1,1,1,1,5,5,1,2,2,2,2,1,1,2,1,1,1,1,1,3,1,1,1,2,3,1,5,1,1,1,2,2,1,1,1,1,1,3,2,1,1,1,4,3,1,1,4,1,5,4,1,4,1,1,1,1,1,1,1,1,1,1,2,2,4,5,1,1,1,1,5,4,1,3,1,1,1,1,4,3,3,3,1,2,3,1,1,1,1,1,1,1,1,2,1,1,1,5,1,3,1,4,3,1,3,1,5,1,1,1,1,3,1,5,1,2,4,1,1,4,1,4,4,2,1,2,1,3,3,1,4,4,1,1,3,4,1,1,1,2,5,2,5,1,1,1,4,1,1,1,1,1,1,3,1,5,1,2,1,1,1,1,1,4,4,1,1,1,5,1,1,5,1,2,1,5,1,1,1,1,1,1,1,1,1,1,1,1,3,2,4,1,1,2,1,1,3,2])
-
-  part2_answer = fish_population2.part2
+  school2 = School.from_file("lib/input.txt")
+  part2_answer = school2.part2
   puts "Answer for part 2: #{part2_answer}"
 end
--- 2021/ruby/day6/spec/day6_spec.rb
+++ 2021/ruby/day6/spec/day6_spec.rb
@@ -1,11 +1,11 @@
 require "day6"
 
-describe FishPopulation do
+describe School do
   context "#from_file" do
     subject { described_class.from_file("spec/test_input.txt") }
 
     it "creates array of fish days" do
-      expect(subject.fish_population).to include({
+      expect(subject.population).to include({
         0 => 0,
         1 => 1,
         2 => 1,
@@ -23,184 +23,68 @@ describe FishPopulation do
     let(:start_population) { [3, 4, 3, 1, 2] }
     subject { described_class.new(start_population) }
 
-    context "answering part 1" do
-      context "#simulate_day" do
-
-        it "updates counts for each fish after one day" do
-          subject.simulate_day
-          expect(subject.fish_population).to include({
-            0 => 1,
-            1 => 1,
-            2 => 2,
-            3 => 1,
-            4 => 0,
-            5 => 0,
-            6 => 0,
-            7 => 0,
-            8 => 0,
-          })
-        end
-
-        it "updates counts for each fish after two days" do
-          2.times { subject.simulate_day }
-          expect(subject.fish_population).to include({
-            0 => 1,
-            1 => 2,
-            2 => 1,
-            3 => 0,
-            4 => 0,
-            5 => 0,
-            6 => 1,
-            7 => 0,
-            8 => 1,
-          })
-        end
-
-        it "handles changes for all 11 days" do
-          11.times { subject.simulate_day }
-          expect(subject.fish_population).to include({
-            0 => 2,
-            1 => 2,
-            2 => 1,
-            3 => 0,
-            4 => 1,
-            5 => 1,
-            6 => 4,
-            7 => 1,
-            8 => 3,
-          })
-        end
-
-        it "handles changes for all 18 days" do
-          18.times { subject.simulate_day }
-          expect(subject.fish_population).to include({
-            0 => 3,
-            1 => 5,
-            2 => 3,
-            3 => 2,
-            4 => 2,
-            5 => 1,
-            6 => 5,
-            7 => 1,
-            8 => 4,
-          })
-        end
+    context "#simulate_day" do
+      it "updates counts for each fish after one day" do
+        subject.simulate_day
+        expect(subject.population).to include({
+          0 => 1,
+          1 => 1,
+          2 => 2,
+          3 => 1,
+          4 => 0,
+          5 => 0,
+          6 => 0,
+          7 => 0,
+          8 => 0,
+        })
       end
 
-      context "#part1" do
-        it "counts the fish population after 18 days" do
-          days_to_simulate = 18
-          expect(subject.part1(18)).to eq(26)
-        end
-
-        it "counts the fish population after 80 days" do
-          expect(subject.part1).to eq(5934)
-        end
+      it "updates counts for two days, including spawning a new one" do
+        2.times { subject.simulate_day }
+        expect(subject.population).to include({
+          0 => 1,
+          1 => 2,
+          2 => 1,
+          3 => 0,
+          4 => 0,
+          5 => 0,
+          6 => 1,
+          7 => 0,
+          8 => 1,
+        })
       end
 
-      context "#part2" do
-        it "counts the fish population after 256 days" do
-          expect(subject.part2).to eq(26984457539)
-        end
+      it "handles changes for all 18 days" do
+        18.times { subject.simulate_day }
+        expect(subject.population).to include({
+          0 => 3,
+          1 => 5,
+          2 => 3,
+          3 => 2,
+          4 => 2,
+          5 => 1,
+          6 => 5,
+          7 => 1,
+          8 => 4,
+        })
+      end
+    end
 
-        it "counts the fish population after 80 days" do
-          expect(subject.part1).to eq(5934)
-        end
+    context "#part1" do
+      it "counts the population after 18 days" do
+        days_to_simulate = 18
+        expect(subject.part1(18)).to eq(26)
+      end
+
+      it "counts the population after 80 days" do
+        expect(subject.part1).to eq(5934)
+      end
+    end
+
+    context "#part2" do
+      it "counts the fish population after 256 days" do
+        expect(subject.part2).to eq(26984457539)
       end
     end
   end
-#   context "answering each part" do
-#     let(:start_population) { [3,4,3,1,2] }
-#     subject { described_class.new(start_population) }
-#
-#     context "answering part 1" do
-#       context "#simulate_day" do
-#
-#         it "decrements day for each fish after one day" do
-#           subject.simulate_day
-#           expect(subject.fish_population).to match_array([2,3,2,0,1])
-#         end
-#
-#         it "decrements again and spawns new fish after two days" do
-#           2.times { subject.simulate_day }
-#           expect(subject.fish_population).to match_array([1, 2, 1, 6, 0, 8])
-#         end
-#
-#         it "handles changes for all 18 days" do
-#           18.times { subject.simulate_day }
-#           expect(subject.fish_population).to match_array([
-#             6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8,
-#           ])
-#         end
-#       end
-#       context "#part1" do
-#         it "counts the fish population after 18 days" do
-#           days_to_simulate = 18
-#           expect(subject.part1(18)).to eq(26)
-#         end
-#
-#         it "counts the fish population after 80 days" do
-#           expect(subject.part1).to eq(5934)
-#         end
-#       end
-#
-#       context "#part1_better_hopefully" do
-#         it "counts the fish population after 18 days" do
-#           days_to_simulate = 18
-#           expect(subject.part1_better_hopefully(days_to_simulate)).to eq(26)
-#         end
-#
-#         it "counts the fish population after 80 days" do
-#           days_to_simulate = 80
-#           expect(subject.part1_better_hopefully(days_to_simulate)).to eq(5934)
-#         end
-#       end
-#     end
-#
-#     context "answering part 2" do
-#       it "counts the fish population after 256 days" do
-#         expect(subject.part2).to eq(26984457539)
-#       end
-#     end
-#   end
-# end
-#
-# describe "Stuff" do
-#   it "gets the correct empty hash" do
-#     expect(figure_it_out).to include({
-#       0 => 0,
-#       1 => 0,
-#       2 => 0,
-#       3 => 0,
-#       4 => 0,
-#       5 => 0,
-#       6 => 0,
-#       7 => 0,
-#       8 => 0,
-#     })
-#   end
-#   # it "simulates population simply" do
-#   #   expect(simulate(
-#   #     [0,1,0,5,6,0,1,2,2,3,0,1,2,2,2,3,3,4,4,5,7,8], 1
-#   #   )).to eq(
-#   #     [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8]
-#   #   )
-#   # end
-#   #
-#   # it "ret8urns poopulation if xzero days given" do
-#   #   expect(simulate([3,4,3,1,2], 0)).to match_array([3,4,3,1,2])
-#   # end
-#   #
-#   # it "simulates another population" do
-#   #   expect(simulate([3,4,3,1,2], 2)).to match_array([1, 2, 1, 6, 0, 8])
-#   # end
-#   #
-#   # it "returns poopulation if xzero days given" do
-#   #   expect(simulate([3,4,3,1,2], 1)).to match_array([2,3,2,0,1])
-#   # end
-#   #
-#   # it "returns poopulation if 18 days given" do
-#   #   expect(
-#   #   expect(simulate([3,4,3,1,2], 18)).to match_array([2,3,2,0,1])
-#   # end
 end
