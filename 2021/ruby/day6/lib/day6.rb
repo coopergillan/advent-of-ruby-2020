db4841a57c99c33387a07b6317bbf47d5ae5f474
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-06 15:31:23 +0000

    Day 6 Part 1 Solution - that's a lotta fish
    Add a solution for part 1 to simulate the fish population using a
    straightforward array for the population and no separate latnernfish
    class (yet???)

--- 2021/ruby/day6/lib/day6.rb
+++ 2021/ruby/day6/lib/day6.rb
@@ -1,17 +1,37 @@
 class FishPopulation
-  attr_accessor :fish
+  attr_accessor :fish_population
 
-  def initialize(fish)
-    @fish = fish
+  def initialize(fish_population)
+    @fish_population = fish_population
   end
 
   def self.from_file(filepath)
-    # fish = []
-    fish  =  File.read(filepath, chomp:true).split(",").map(&:to_i)
-    new(fish)
+    fish_population = File.read(filepath, chomp:true).split(",").map(&:to_i)
+    new(fish_population)
   end
 
-  def part1
+  def simulate_day
+    new_fish_count = 0
+    @fish_population.each_with_index do |fish, idx|
+      # puts "Checking fish: #{fish} - which should match @fish_population[idx]: #{@fish_population[idx]}"
+      internal_timer = @fish_population[idx]
+      if internal_timer.zero?
+        new_fish_count +=1
+        internal_timer = 6
+      else
+        internal_timer -= 1
+      end
+      @fish_population[idx] = internal_timer
+      # puts "Finished checking fish: #{fish} - which should match @fish_population[idx]: #{@fish_population[idx]}"
+    end
+    if !new_fish_count.zero?
+      new_fish_count.times { @fish_population.push(8) }
+    end
+  end
+
+  def part1(days = 80)
+    days.times { simulate_day }
+    @fish_population.size
   end
 
   def part2
@@ -21,8 +41,8 @@ end
 
 if $PROGRAM_NAME  == __FILE__
   fish_population1 = FishPopulation.from_file("lib/input.txt")
-  # part1_answer = fish_population1.part1
-  # puts "Answer for part 1: #{part1_answer}"
+  part1_answer = fish_population1.part1
+  puts "Answer for part 1: #{part1_answer}"
 
   # fish_population2 = FishPopulation.from_file("lib/input.txt")
   # part2_answer = fish_population2.part2
--- 2021/ruby/day6/spec/day6_spec.rb
+++ 2021/ruby/day6/spec/day6_spec.rb
@@ -5,13 +5,43 @@ describe FishPopulation do
 
   context "#from_file" do
     it "creates array of fish days" do
-      expect(subject.fish).to match_array([
-        3,4,3,1,2
-      ])
+      expect(subject.fish_population).to match_array([3,4,3,1,2])
     end
   end
 
-  context "answering part 1"
+  context "answering part 1" do
+    let(:start_population) { [3,4,3,1,2] }
+    subject { described_class.new(start_population) }
+    context "#simulate_day" do
+
+      it "decrements day for each fish after one day" do
+        subject.simulate_day
+        expect(subject.fish_population).to match_array([2,3,2,0,1])
+      end
+
+      it "decrements again and spawns new fish after two days" do
+        2.times { subject.simulate_day }
+        expect(subject.fish_population).to match_array([1, 2, 1, 6, 0, 8])
+      end
+
+      it "handles changes for all 18 days" do
+        18.times { subject.simulate_day }
+        expect(subject.fish_population).to match_array([
+          6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8,
+        ])
+      end
+    end
+    context "#part1" do
+      it "counts the fish population after 18 days" do
+        days_to_simulate = 18
+        expect(subject.part1(18)).to eq(26)
+      end
+
+      it "counts the fish population after 80 days" do
+        expect(subject.part1).to eq(5934)
+      end
+    end
+  end
 
   context "answering part 2"
 
