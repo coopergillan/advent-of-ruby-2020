eff5923968f737e126c6ddf541e5db24a423edf3
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-07 15:18:15 +0000

    Day 7 Final cleanup - yield to being content
    Get the two integers surrounding the floating point mean position and
    map to the smaller of the two to calculate the most efficient fuel.
    After attempting to use a yield keyword in the same way that Python
    does, it became apparent that Ruby uses this differently. While it would
    be great to dive into it more, that will be reserved for another day
    while these results are yielded to.

--- 2021/ruby/day7/lib/day7.rb
+++ 2021/ruby/day7/lib/day7.rb
@@ -10,18 +10,6 @@ class CrabSchool
     new(positions)
   end
 
-  def calculate_fuel(final_position)
-    @positions.reduce(0) do |fuel, position|
-      fuel + (position - final_position).abs
-    end
-  end
-
-  def calculate_new_fuel(final_position)
-    @positions.reduce(0) do |fuel, position|
-      fuel + fuel_for_move(position, final_position)
-    end
-  end
-
   def fuel_for_move(start_position, end_position)
     moves = (end_position - start_position).abs
     (0..moves).reduce(:+)
@@ -32,37 +20,39 @@ class CrabSchool
   end
 
   def part2
-    position_to_use = mean_position
-    start = mean_position - 10
-    final = mean_position + 10
-    results = []
-    (start..final).each do |final_position|
-      results << calculate_new_fuel(final_position)
-    end
-    results.min
-    # puts "Got mean_position: #{mean_position}"
-		# calculate_new_fuel(mean_position)
+    mean_positions.map { |p| calculate_new_fuel(p) }.min
   end
 
   private
 
-  def mean_position
-    (@positions.reduce(:+) / @positions.size.to_f).round
+  def calculate_fuel(final_position)
+    @positions.reduce(0) do |fuel, position|
+      fuel + (position - final_position).abs
+    end
   end
 
   def median_position
     @positions.sort[@positions.size / 2]
   end
 
+  def calculate_new_fuel(final_position)
+    @positions.reduce(0) do |fuel, position|
+      fuel + fuel_for_move(position, final_position)
+    end
+  end
+
+  def mean_positions
+    raw_mean = (@positions.reduce(:+) / @positions.size.to_f)
+
+    [raw_mean.floor, raw_mean.round]
+  end
+
 end
 
 
 if $PROGRAM_NAME  == __FILE__
-  school1 = CrabSchool.from_file("lib/input.txt")
-  part1_answer = school1.part1
-  puts "Answer for part 1: #{part1_answer}"
+  crab_school = CrabSchool.from_file("lib/input.txt")
 
-  school2 = CrabSchool.from_file("lib/input.txt")
-  part2_answer = school2.part2
-  puts "Answer for part 2: #{part2_answer}"
+  puts "Answer for part 1: #{crab_school.part1}"
+  puts "Answer for part 2: #{crab_school.part2}"
 end
