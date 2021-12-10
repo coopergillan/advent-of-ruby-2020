52a39519d5e864bbe6e2b7ed1d0ce2da67004c96
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-10 04:49:31 +0000

    WIP starting  the procedural COBOL machine

--- 2021/ruby/day8/lib/day8.rb
+++ 2021/ruby/day8/lib/day8.rb
@@ -15,26 +15,100 @@ class Solver
   end
 end
 
-class SignalCombo
+class DigitSignal
   attr_accessor :chars
+
+  FIXED_SIGNAL_LENGTHS = {
+    2 => 1,
+    3 => 7,
+    4 => 4,
+    # 5 => [2, 3, 5],
+    # 6 => [0, 6, 9],
+    7 => 8,
+  }
 
   def initialize(chars)
     @chars = chars
   end
 
-  def char_count
+  def chars_length
     @chars.size
   end
 
-  def unique_chars_count
-    unique_chars = [2, 3, 4, 7]
-    @chars.map { |char| (unique_chars.include?(char.size) ? 1 : 0) }.reduce(:+)
+  def shares(other_digit_signal)
+    # Which characters are shared between two
+    @chars.each_char.to_i & other_digit_signal.chars.each_char.to_a
+  end
+
+  def decode
+    FIXED_SIGNAL_LENGTHS[chars_length]
+  end
+end
+
+class SignalCombo
+  attr_accessor :signal_patterns, :output_values, :numbers_hash
+
+  FIXED_SIGNAL_LENGTHS = {
+    2 => 1,
+    3 => 7,
+    4 => 4,
+    # 5 => [2, 3, 5],
+    # 6 => [0, 6, 9],
+    7 => 8,
+  }
+  def initialize(signal_patterns, output_values)
+    @signal_patterns = signal_patterns
+    @output_values = output_values
+    @numbers_hash = {}
   end
 
   def self.from_raw(raw_input)
-    new(raw_input.split("|").last.split)
+    raw_signal_patterns, raw_output_values = raw_input.split("|")
+
+    signal_patterns = raw_signal_patterns.split
+    output_values = raw_output_values.split
+    new(signal_patterns, output_values)
   end
 
+  def sort_chars(chars)
+    # require "pry"; binding.pry
+    chars.each_char.to_a.sort.join
+  end
+
+  def arrayify(chars)
+    # Turn chars into an array for array arithmetic
+    chars.each_char.to_a
+  end
+
+  def build_hash
+    @signal_patterns.each do |pattern|
+
+      # Populate the four fixed lengths for use in the rest, then come back through =|
+      if fixed_length = FIXED_SIGNAL_LENGTHS[pattern.size]
+        @numbers_hash[pattern] = fixed_length
+      end
+    end
+
+    @signal_patterns.each do |pattern|
+      # Find six character ones
+      if pattern.size == 6
+        # Find the number six - should be missing a character that does not contain both chars from 1
+        if !sort_chars(pattern).include?(sort_chars(@numbers_hash.invert[1]))
+          @numbers_hash[pattern] = pattern.size
+        # Find zero - only diff between it and 8 should be one character in 4
+        elsif middle_section = (arrayify(@numbers_hash.invert[8]) - arrayify(pattern)).join
+          # require "pry"; binding.pry
+          @numbers_hash.invert[4].include?(middle_section)
+          @numbers_hash[pattern] = 0
+        end
+      end
+    end
+  end
+
+  def unique_chars_count
+    # unique_chars = [2, 3, 4, 7]
+    @output_values.map { |char| (FIXED_SIGNAL_LENGTHS.keys.include?(char.size) ? 1 : 0) }.reduce(:+)
+  end
 end
 
 if $PROGRAM_NAME  == __FILE__
@@ -45,3 +119,13 @@ if $PROGRAM_NAME  == __FILE__
   # part2_answer = school2.part2
   # puts "Answer for part 2: #{part2_answer}"
 end
+
+# DIGIT_PARTS = [
+#   :top,
+#   :top_left,
+#   :top_right,
+#   :middle,
+#   :bottom_left,
+#   :bottom_right,
+#   :bottom,
+# ]
--- 2021/ruby/day8/spec/day8_spec.rb
+++ 2021/ruby/day8/spec/day8_spec.rb
@@ -9,7 +9,84 @@ describe SignalCombo do
       ])
     end
     it "gets number of chars that represent with 1, 4, 7, or 8" do
-      # require "pry"; binding.pry
+      expect(subject.unique_chars_count).to eq(2)
+    end
+  end
+end
+
+describe DigitSignal do
+  let (:signals) {
+    {
+    8 => "acedgfb", # "abcdefg"
+    5=> "cdfbe",    # "bcdef"
+    2=> "gcdfa",    # "acdfg"
+    3=> "fbcad",    # "fbcad"
+    7=> "dab",      # "dab"
+    9=> "cefabd",   # "cefabd"
+    6=> "cdfgeb",   # "cdfgeb"
+    4=> "eafb",     # "eafb"
+    0=> "cagedb",   # "cagedb"
+    1=> "ab",       # "ab"
+    }
+  }
+  context "#decode" do
+    it "decodes the simpler, unique combos" do
+      expect(described_class.new("ag").decode).to eq(1)
+      expect(described_class.new("agf").decode).to eq(7)
+      expect(described_class.new("decb").decode).to eq(4)
+      expect(described_class.new("acedgfb").decode).to eq(8)
+    end
+
+    it "can find shared characters" do
+      expect(described_class.new("agf")).to eq(7)
+    end
+    xit "decodes the combos with other lengths" do
+      expect(described_class.new("acdgfb").decode).to eq(0)
+      expect(described_class.new("acdgfb").decode).to eq(0)
+    end
+  end
+end
+
+describe SignalCombo do
+  subject { described_class.from_raw("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf") }
+  context "#new" do
+    it "gets attributes" do
+      expect(subject.signal_patterns).to match_array(
+        ["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"],
+      )
+      expect(subject.output_values).to match_array(
+        ["cdfeb", "fcadb", "cdfeb", "cdbaf"],
+      )
+    end
+  end
+
+  context "helper methods" do
+    it "sorts characters for easier comparison" do
+      expect(subject.sort_chars("gabcdef")).to eq("abcdefg")
+    end
+    it "converts characters into arrays for easier sorting" do
+      expect(subject.arrayify("gabcdef")).to match_array([
+        "g", "a", "b", "c", "d", "e", "f",
+      ])
+    end
+  end
+
+  context "#build_hash" do
+    it "builds a hash for the four unique characters" do
+      subject.build_hash
+      expect(subject.numbers_hash).to include(
+        "cagedb" => 0,
+        "ab" => 1,
+        "eafb" => 4,
+        "cdfgeb" => 6,
+        "dab" => 7,
+        "acedgfb" => 8,
+      )
+    end
+  end
+
+  context "#unique_chars_count" do
+    it "gets number of chars that represent with 1, 4, 7, or 8" do
       expect(subject.unique_chars_count).to eq(2)
     end
   end
@@ -22,6 +99,3 @@ describe Solver do
     expect(subject.part1).to eq(26)
   end
 end
-
-# describe Solver do
-# end
