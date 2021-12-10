5b06b3c882cfa7299e6aa587fec133b2fae3dd52
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-10 21:05:03 +0000

    Day 8 final cleanup
    Clean up the code and tests to remove debugging statements, improve
    naming, move more logic into code blocks, and consolidate the tests.
    
    Not really proud of how this turned out, but still proud to get the
    answer and have SOME semblance of logic around it =|

--- 2021/ruby/day8/lib/day8.rb
+++ 2021/ruby/day8/lib/day8.rb
@@ -15,15 +15,7 @@ class Solver
   end
 
   def part2
-    thesum = 0
-    @signal_combos.each do |combo|
-      puts "Checking combo: #{combo}"
-      combo.build_hash
-      output_number = combo.output_to_number
-      puts "Got output_number: #{output_number}"
-      thesum += output_number
-    end
-    thesum
+    @signal_combos.map { |combo| combo.output_to_number }.reduce(:+)
   end
 end
 
@@ -50,13 +42,13 @@ class SignalCombo
     new(signal_patterns, output_values)
   end
 
-  def sort_chars(chars)
-    chars.each_char.to_a.sort.join
-  end
-
   def arrayify(chars)
     # Turn chars into an array for array arithmetic
     chars.each_char.to_a
+  end
+
+  def char_intersection_size(chars, chars_to_compare)
+    arrayify(chars).intersection(arrayify(chars_to_compare)).size
   end
 
   def build_hash
@@ -71,119 +63,63 @@ class SignalCombo
       digit_to_chars[known] = sorted_signals.shift
     end
 
+    # Pull some known numbers out for comparing later
+    one, four, eight = digit_to_chars.values_at(1, 4, 8)
+
     # Pull six-character chars out
     sixes = sorted_signals.pop(3)
-    cycle_count = 0
     sixes.cycle do |chars|
 
       # Find 9 - it's the only six-character one that has all of four's characters
-      char_to_four_intersection = arrayify(chars).intersection(arrayify(digit_to_chars[4]))
-      # All of the characters match to four
-      if char_to_four_intersection.size == digit_to_chars[4].size
-        puts "Checking chars: #{chars}"
-        puts "char_to_four_intersection: #{char_to_four_intersection}"
-        puts "char_to_four_intersection.size: #{char_to_four_intersection.size}"
-        puts "digit_to_chars[4]: #{digit_to_chars[4]}"
-        puts "digit_to_chars[4].size: #{digit_to_chars[4].size}"
-        puts "FOUNDNINENENENEN????????????"
-        puts "about to add to digit_to_chars: #{digit_to_chars}"
-        puts "Pulling form sixes: #{sixes}"
+      if char_intersection_size(chars, four) == four.size
         digit_to_chars[9] = sixes.delete(chars)
-        puts "Added nine - now have digit_to_chars: #{digit_to_chars}"
-        puts "digit_to_chars[9]: #{digit_to_chars[9]}"
-        puts "============================"
       end
 
-      # Find number 6
-      # Make sure that 9 is already found?
-      if sixes.size == 2
-      # - does not share all of 1 so the intersection will only have one char
-        char_to_one_intersection = arrayify(chars).intersection(arrayify(digit_to_chars[1]))
-        if char_to_one_intersection.size != digit_to_chars[1].size
-          puts "Checking chars: #{chars}"
-          puts "char_to_one_intersection: #{char_to_one_intersection}"
-          puts "about to add 6 to digit_to_chars - here is before: #{digit_to_chars}"
-          puts "digit_to_chars[6]: #{digit_to_chars[6]}"
-          puts "Pulling from sixes to get 6: #{sixes}"
-          digit_to_chars[6] = sixes.delete(chars)
-          puts "Pulled from sixes: #{sixes}"
-          puts "============================"
-        end
+      # Find 6 - does not share all of 1 so the intersection will only have one char
+      if char_intersection_size(chars, one) != one.size
+        digit_to_chars[6] = sixes.delete(chars)
       end
 
-      # sorted_nine = sort_chars(chars)
-      # puts "Got sorted nine: #{sorted_nine}"
-      # sorted_four = sort_chars(digit_to_chars[4])
-      # puts "Got sorted four: #{sorted_four}"
-      # if arrayify(chars).include?(sort_chars(digit_to_chars[4]))
-      #   puts "Found 9, right? #{chars}"
-      #   digit_to_chars[9] = sixes.delete(chars)
-      # end
-      cycle_count += 1
-      # puts "Finished checking #{chars}"
-      # puts "Finished the cycle! Run number #{cycle_count}"
-      # puts "digit_to_chars: #{digit_to_chars}"
-      # puts "============================="
-      if sixes.one?
-        # Double check that zero contains all of one
-        char_to_one_intersection = arrayify(chars).intersection(arrayify(digit_to_chars[1]))
-        if char_to_one_intersection.size == digit_to_chars[1].size
-          puts "Checking chars for 0: #{chars}"
-          puts "char_to_one_intersection: #{char_to_one_intersection}"
-          puts "Just one left in sixes: #{sixes}"
-          puts "It must be zero????? Here is digit_to_chars before adding: #{digit_to_chars}"
-          digit_to_chars[0] = sixes.pop
-          puts "Added zero???? - now have digit_to_chars: #{digit_to_chars}"
-          puts "digit_to_chars[0]: #{digit_to_chars[0]}"
-        end
-      end
+      # 0 can be added when it is the only one left over
+      digit_to_chars[0] = sixes.pop if sixes.one?
     end
 
     # Now pull the fives out and do more elimination
     fives = sorted_signals.pop(3)
-
     fives.cycle do |chars|
-      # Find the number 3 - it's the only five-digit that has both of 1's chars
-      char_to_one = arrayify(chars).intersection(arrayify(digit_to_chars[1]))
-      # puts "char_to_one: #{char_to_one}"
-      # puts "digit_to_chars: #{digit_to_chars}"
-      if char_to_one.size == digit_to_chars[1].size
+
+      # Find 3 - the only five-digit char that has both of 1's chars
+      if char_intersection_size(chars, one) == one.size
         digit_to_chars[3] = fives.delete(chars)
       end
 
       # Now find 2 - it should contain all of the difference between 8 and four (while five does not)
-      eight_to_four_diff = arrayify(digit_to_chars[8]) - arrayify(digit_to_chars[4])
-      # puts "eight_to_four_diff: #{eight_to_four_diff}"
-      if arrayify(chars).intersection(eight_to_four_diff).size == eight_to_four_diff.size
+      eight_minus_four_chars = (arrayify(eight) - arrayify(four)).join
+      if char_intersection_size(chars, eight_minus_four_chars) == eight_minus_four_chars.size
         digit_to_chars[2] = fives.delete(chars)
       end
 
-      # 5 is left over
-      if fives.one?
-        digit_to_chars[5] = fives.pop
-      end
+      # 5 is the only one left now
+      digit_to_chars[5] = fives.pop if fives.one?
     end
 
-    puts "finished mapping the chars - digit_to_chars: #{digit_to_chars}"
+    # Now flip the hash in order to decode the output values
     @numbers_hash = digit_to_chars.invert
-    raise if @numbers_hash.size != 10
   end
 
   def output_to_number
-    puts "Checking @numbers_hash: #{@numbers_hash}"
+    build_hash
+
     sorted_key_numbers_hash = {}.tap do |hash|
       @numbers_hash.each do |chars, digit|
-        puts "chars: #{chars}"
-        puts "digit: #{digit}"
-        puts "=================="
         hash[chars.each_char.sort.join] = digit
       end
     end
-    output_string = @output_values.reduce("") do |num, val|
+
+    @output_values.reduce("") do |num_string, val|
       sorted_val = val.each_char.sort.join
-      num += sorted_key_numbers_hash[sorted_val].to_s
-    end
-    output_string.to_i
+      num_string += sorted_key_numbers_hash[sorted_val].to_s
+    end.to_i
   end
 
   def unique_chars_count
@@ -193,8 +129,7 @@ end
 
 if $PROGRAM_NAME  == __FILE__
   solver = Solver.from_file("lib/input.txt")
-  puts "Answer for part 1: #{solver.part1}"
 
-  solver2 = Solver.from_file("lib/input.txt")
-  puts "Answer for part 2: #{solver2.part2}"
+  puts "Answer for part 1: #{solver.part1}"
+  puts "Answer for part 2: #{solver.part2}"
 end
--- 2021/ruby/day8/spec/day8_spec.rb
+++ 2021/ruby/day8/spec/day8_spec.rb
@@ -16,10 +16,12 @@ describe Solver do
 end
 
 describe SignalCombo do
+  let(:raw_input) {
+    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
+  }
+  subject { described_class.from_raw(raw_input) }
   context "#new" do
-    subject { described_class.from_raw("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe") }
-
-    it "gets attributes" do
+    it "gets attributes" do  # Yeah, yeah, not really needed but some assurance was needed =)
       expect(subject.signal_patterns).to match_array([
         "be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb",
       ])
@@ -27,77 +29,74 @@ describe SignalCombo do
         "fdgacbe", "cefdb", "cefbgd", "gcbe",
       ])
     end
-    it "gets number of chars that represent with 1, 4, 7, or 8" do
-      expect(subject.unique_chars_count).to eq(2)
-    end
-
-    it "builds the correct numbers hash" do
-      subject.build_hash
-      expect(subject.numbers_hash).to include(
-        "agebfd" => 0,
-        "be" => 1,
-        "fabcd" => 2,
-        "fecdb" => 3,
-        "cgeb" => 4,
-        "fdcge" => 5,
-        "fgaecd" => 6,
-        "edb" => 7,
-        "cfbegad" => 8,
-        "cbdgef" => 9,
-      )
-    end
-  end
-end
-
-describe SignalCombo do
-  subject { described_class.from_raw(
-    "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
-  ) }
-
-  context "#new" do
-    it "gets attributes" do
-      expect(subject.signal_patterns).to match_array(
-        ["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"],
-      )
-      expect(subject.output_values).to match_array(
-        ["cdfeb", "fcadb", "cdfeb", "cdbaf"],
-      )
-    end
   end
 
   context "helper methods" do
-    it "sorts characters for easier comparison" do
-      expect(subject.sort_chars("gabcdef")).to eq("abcdefg")
-    end
     it "converts characters into arrays for easier sorting" do
       expect(subject.arrayify("gabcdef")).to match_array([
         "g", "a", "b", "c", "d", "e", "f",
       ])
     end
-  end
 
-  context "#build_hash" do
-    it "builds a hash for the four unique characters" do
-      subject.build_hash
-      expect(subject.numbers_hash).to include(
-        "cagedb" => 0,
-        "ab" => 1,
-        "gcdfa" => 2,
-        "fbcad" => 3,
-        "eafb" => 4,
-        "cdfbe" => 5,
-        "cdfgeb" => 6,
-        "dab" => 7,
-        "acedgfb" => 8,
-        "cefabd" => 9,
-      )
+    it "gets the intersection of two chars as an array" do
+      expect(subject.char_intersection_size("gabcdef", "cg")).to eq(2)
+      expect(subject.char_intersection_size("cgfa", "gabcdef")).to eq(4)
     end
   end
 
-  context "answering part 2" do
-    it "parses the output into a four-digit number" do
-      subject.build_hash
-      expect(subject.output_to_number).to eq(5353)
+  context "it helps answer the main questions" do
+    context "when using the original example" do
+      it "gets number of chars that represent with 1, 4, 7, or 8" do
+        expect(subject.unique_chars_count).to eq(2)
+      end
+
+      it "builds the correct numbers hash with first example" do
+        subject.build_hash
+        expect(subject.numbers_hash).to include(
+          "agebfd" => 0,
+          "be" => 1,
+          "fabcd" => 2,
+          "fecdb" => 3,
+          "cgeb" => 4,
+          "fdcge" => 5,
+          "fgaecd" => 6,
+          "edb" => 7,
+          "cfbegad" => 8,
+          "cbdgef" => 9,
+        )
+      end
+
+      it "parses the output into a four-digit number for use by #part2" do
+        expect(subject.output_to_number).to eq(8394)
+      end
+    end
+
+    context "when using another given example" do
+      context "when answering part 2" do
+        let(:raw_input) {
+          "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+        }
+
+        it "builds correct numbers hash with another example" do
+          subject.build_hash
+          expect(subject.numbers_hash).to include(
+            "cagedb" => 0,
+            "ab" => 1,
+            "gcdfa" => 2,
+            "fbcad" => 3,
+            "eafb" => 4,
+            "cdfbe" => 5,
+            "cdfgeb" => 6,
+            "dab" => 7,
+            "acedgfb" => 8,
+            "cefabd" => 9,
+          )
+        end
+
+        it "parses the output into a four-digit number for use by #part2" do
+          expect(subject.output_to_number).to eq(5353)
+        end
+      end
     end
   end
 end
