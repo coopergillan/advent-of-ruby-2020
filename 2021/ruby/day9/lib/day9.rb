52702276300b98f6859ce9100d01f95ec5dc1f9c
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-14 04:13:34 +0000

    More work - little progress

--- 2021/ruby/day9/lib/day9.rb
+++ 2021/ruby/day9/lib/day9.rb
@@ -15,12 +15,19 @@ class LavaTubeSurfer
   end
 
   def neighbors(coordinates)
+    raw_neighbor_coords(coordinates).map do |(row_check, column_check)|
+      next unless row_check.between?(0, @height - 1) && column_check.between?(0, @width - 1)
+        @heightmap[row_check][column_check]
+    end.compact
+  end
+
+  def raw_neighbor_coords(coordinates)
     row, column = coordinates
 
     # TODO: find some Ruby helper methods like each_cons that can derive
     # these neighbors to each coordinate
     # Check the row
-    puts "Running neighbors for row: #{row} and column: #{column}"
+    # puts "Running neighbors for row: #{row} and column: #{column}"
     previous_row = row - 1
     next_row = row + 1
 
@@ -28,16 +35,14 @@ class LavaTubeSurfer
     previous_column = column - 1
     next_column = column + 1
 
-    neighbors = [
+    [
       [row, previous_column],
       [previous_row, column],
       [row, next_column],
       [next_row, column],
-    ]
-
-    neighbors.map do |(row_check, column_check)|
+    ].map do |(row_check, column_check)|
       next unless row_check.between?(0, @height - 1) && column_check.between?(0, @width - 1)
-        @heightmap[row_check][column_check]
+      [row_check, column_check]
     end.compact
   end
 
@@ -48,9 +53,7 @@ class LavaTubeSurfer
 
   def risk_level(coordinates)
     row, column = coordinates
-    if higher_neighbors?(coordinates)
-      return @heightmap[row][column] + 1
-    end
+    return @heightmap[row][column] + 1 if higher_neighbors?(coordinates)
     0
   end
 
@@ -60,12 +63,75 @@ class LavaTubeSurfer
       row.each_with_index.map do |_, col_idx|
         # risk_level([row_idx, col_idx])
         this_level = risk_level([row_idx, col_idx])
-        puts "Calculated this_level: #{this_level}"
+        # puts "Calculated this_level: #{this_level}"
         risk_level_sum += this_level
       end
     end
     risk_level_sum
   end
+
+  def in_basin?(coordinates)
+    row, column = coordinates
+    return false if @heightmap[row][column] == 9
+    true
+  end
+
+  def basin_size(coordinates, visited = [])
+    puts "Checkingn coords: #{coordinates}"
+    unvisited = coordinates - visited
+    puts "unvisited: #{unvisited}"
+    return if unvisited.empty?
+    visited << coordinates
+    puts "visited: #{visited}"
+    return in_basin?(coordinates) ? 1 : 0
+
+    if (raw_neighbors = raw_neighbor_coords(coordinates))
+      neighbors_to_check = raw_neighbors - visited
+    else
+      neighbors_to_check = []
+    end
+    found_neighbors = neighbors_to_check.map do |neighbor_coords|
+      basin_size(neighbor_coords, visited)
+      puts ""
+    end
+    return 1
+    puts "found_neighbors: #{found_neighbors}"
+  end
+
+  # def basin_size(coordinates, size = 0, visited = [])
+  #   unvisited = coordinates - visited
+  #   puts "unvisited: #{unvisited}"
+  #   return if unvisited.empty?
+  #   visited << coordinates
+  #   puts "Checking basin_size for coordinates: #{coordinates}"
+  #   puts "size is: #{size}"
+  #   puts "visited is: #{visited}"
+  #   if !in_basin?(coordinates)
+  #     puts "coordinates: #{coordinates} are not in a basin"
+  #     puts "returning size: #{size}"
+  #     return 0
+  #   end
+  #   puts "coordinates: #{coordinates} are in a basin"
+  #
+  #   # Increment by 1 if it is in a basin
+  #   size = 1
+  #
+  #   if (raw_neighbors = raw_neighbor_coords(coordinates))
+  #     neighbors_to_check = raw_neighbors - visited
+  #   else
+  #     neighbors_to_check = []
+  #   end
+  #   puts "Checking for neighbors: #{neighbors_to_check}"
+  #   # neighbors_to_check = raw_neighbor_coords(neighbors_to_check) - visited
+  #   neighbors_to_check.map do |neighbor_coords|
+  #     # puts "Now going to check neighbor_coords: #{neighbor_coords}"
+  #     # puts "Size is at #{size}"
+  #     # break if size >= 16
+  #     basin_size(neighbor_coords, size, visited)
+  #     puts ""
+  #   end
+  #   size
+  # end
 
   def part2
     "wrong again"
--- 2021/ruby/day9/spec/day9_spec.rb
+++ 2021/ruby/day9/spec/day9_spec.rb
@@ -18,15 +18,20 @@ describe LavaTubeSurfer do
   end
 
   context "given a point on the map" do
-    # let(:edge) { [4, 7] }
-    # let(:middle) { [3, 8] }
     context "when checking corners" do
       let(:top_left) { [0, 0] }
       let(:bottom_left) { [4, 0] }
       let(:top_right) { [0, 9] }
       let(:bottom_right) { [4, 9] }
 
-      it "can get the neighboring points" do
+      it "can get the neighboring coordinates" do
+        expect(subject.raw_neighbor_coords(top_left)).to eq([[0, 1], [1, 0]])
+        expect(subject.raw_neighbor_coords(bottom_left)).to eq([[3, 0], [4, 1]])
+        expect(subject.raw_neighbor_coords(top_right)).to eq([[0, 8], [1, 9]])
+        expect(subject.raw_neighbor_coords(bottom_right)).to eq([[4, 8], [3, 9]])
+      end
+
+      it "can get the neighboring values" do
         expect(subject.neighbors(top_left)).to eq([1, 3])
         expect(subject.neighbors(bottom_left)).to eq([8, 8])
         expect(subject.neighbors(top_right)).to eq([1, 1])
@@ -39,6 +44,16 @@ describe LavaTubeSurfer do
       let(:right_border) { [3, 9] }
       let(:top_border) { [0, 6] }
       let(:bottom_border) { [4, 5] }
+
+      it "can get the neighboring coordinates" do
+        expect(subject.raw_neighbor_coords(left_border)).to eq([
+          [1, 0], [2, 1], [3, 0],
+        ])
+        # expect(subject.raw_neighbor_coords(right_border)).to eq([8, 2, 8])
+        # expect(subject.raw_neighbor_coords(top_border)).to eq([4, 2, 4])
+        # expect(subject.raw_neighbor_coords(bottom_border)).to eq([9, 9, 5])
+      end
+
       it "can get the neighboring points" do
         expect(subject.neighbors(left_border)).to eq([3, 8, 8])
         expect(subject.neighbors(right_border)).to eq([8, 2, 8])
@@ -48,9 +63,20 @@ describe LavaTubeSurfer do
     end
 
     context "when checking pieces in the middle" do
+      let(:mid1) { [2, 3] }
+      let(:mid2) { [3, 8] }
+      it "can get the neighboring coordinates" do
+        expect(subject.raw_neighbor_coords(mid1)).to eq([
+          [2, 2], [1, 3], [2, 4], [3, 3],
+        ])
+        expect(subject.raw_neighbor_coords(mid2)).to eq([
+          [3, 7], [2, 8], [3, 9], [4, 8],
+        ])
+      end
+
       it "can get the neighboring points" do
-        expect(subject.neighbors([2, 3])).to eq([5, 7, 7, 7])
-        expect(subject.neighbors([3, 8])).to eq([7, 9, 9, 7])
+        expect(subject.neighbors(mid1)).to eq([5, 7, 7, 7])
+        expect(subject.neighbors(mid2)).to eq([7, 9, 9, 7])
       end
     end
   end
@@ -91,6 +117,28 @@ describe LavaTubeSurfer do
   context "#part1" do
     it "can answer part1 by counting points with all higher neighbors" do
       expect(subject.part1).to eq(15)
+    end
+  end
+
+  context "answering part2" do
+    context "when identifying and sizing basins" do
+      let(:low_point1) { [0, 1] }
+      it "can identify whether a point is in a basin" do
+        expect(subject.in_basin?([0, 0])).to be(false)
+        expect(subject.in_basin?([1, 1])).to be(true)
+      end
+
+      it "returns nil if a point is not a low point" do
+        expect(subject.basin_size([0, 0])).to be(nil)
+      end
+
+      it "counts the size of the basin for a low point" do
+        expect(subject.basin_size(low_point1)).to eq(3)
+      end
+    end
+
+    it "gets the final answer by multiplying the size of three biggest basins together" do
+      expect(subject.part2).to eq(9 * 14 * 9)
     end
   end
 end
