b6b484c314186106fa4774a00c899384430ed2dc
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-16 21:57:34 +0000

    WIP part 1 with updated and working raw neighbors

---
+++ 2021/ruby/day11/lib/day11.rb
@@ -0,0 +1,109 @@
+class Cavern
+  attr_accessor :octopus_map, :height, :width
+
+  def initialize(octopus_map)
+    @octopus_map = octopus_map
+    @height = octopus_map.size
+    @width = octopus_map.first.size
+  end
+
+  def self.from_file(filepath)
+    new(
+      File.foreach(filepath, chomp: true).map { |line| line.each_char.map(&:to_i) }
+    )
+  end
+
+  # def neighbors(coordinates)
+  #   raw_neighbor_coords(coordinates).map do |(row_check, column_check)|
+  #     @heightmap[row_check][column_check]
+  #   end.compact
+  # end
+
+  def raw_neighbor_coords(coordinates)
+    row, column = coordinates
+
+    # TODO: find some Ruby helper methods like each_cons that can derive neighbor coordinates
+    previous_row = row - 1
+    next_row = row + 1
+    previous_column = column - 1
+    next_column = column + 1
+
+    neighbors_check = [
+      [previous_row, previous_column],
+      [previous_row, column],
+      [previous_row, next_column],
+      [row, previous_column],
+      [row, next_column],
+      [next_row, previous_column],
+      [next_row, column],
+      [next_row, next_column],
+    ]
+
+    neighbors_check.map do |(row_check, column_check)|
+      next unless row_check.between?(0, @height - 1) && column_check.between?(0, @width - 1)
+      [row_check, column_check]
+    end.compact
+  end
+
+  def higher_neighbors?(coordinates)
+    row, column = coordinates
+    neighbors(coordinates).map { |neighbor| neighbor > @heightmap[row][column] }.all?
+  end
+
+  def risk_level(coordinates)
+    row, column = coordinates
+    return @heightmap[row][column] + 1 if higher_neighbors?(coordinates)
+    0
+  end
+
+  def part1
+    # TODO: figure out how to derive this using reduce instead of adding to risk_level_sul
+    risk_level_sum = 0
+    @heightmap.each_with_index do |row, row_idx|
+      row.each_with_index.map do |_, col_idx|
+        this_level = risk_level([row_idx, col_idx])
+        risk_level_sum += this_level
+      end
+    end
+    risk_level_sum
+  end
+
+  def basin_size(coordinates, visited = [])
+    unvisited = [coordinates] - visited
+    return if !in_basin?(coordinates) || unvisited.empty?
+
+    visited << coordinates
+    (raw_neighbor_coords(coordinates) - visited).each { |n| basin_size(n, visited) }
+    visited.size
+  end
+
+  def part2
+    # TODO: figure out how to derive `basins` with map
+    basins = []
+    @heightmap.each_with_index do |row, row_idx|
+      row.each_with_index.map do |_, col_idx|
+        coords = [row_idx, col_idx]
+        if higher_neighbors?(coords)
+          basins.push(basin_size(coords))
+        end
+      end
+    end
+    basins.sort.last(3).reduce(1, :*)
+  end
+
+  private
+
+  def in_basin?(coordinates)
+    row, column = coordinates
+    return false if @heightmap[row][column] == 9
+    true
+  end
+end
+
+
+if $PROGRAM_NAME  == __FILE__
+  lava_tube = LavaTubeSurfer.from_file("lib/input.txt")
+
+  puts "Answer for part 1: #{lava_tube.part1}"
+  puts "Answer for part 2: #{lava_tube.part2}"
+end
---
+++ 2021/ruby/day11/spec/day11_spec.rb
@@ -0,0 +1,84 @@
+require "day11"
+
+describe Cavern do
+  subject { described_class.from_file("spec/test_input.txt") }
+
+  context "#new" do
+    it "gets a 10x10 array of octopus energy levels" do
+      expect(subject.octopus_map).to be_a(Array)
+      expect(subject.octopus_map.height).to eq(10)
+      expect(subject.octopus_map.width).to eq(10)
+    end
+  end
+
+  context "given a point on the map" do
+    context "when checking corners" do
+      let(:top_left) { [0, 0] }
+      let(:bottom_left) { [9, 0] }
+      let(:top_right) { [0, 9] }
+      let(:bottom_right) { [9, 9] }
+
+      it "can get the neighboring coordinates" do
+        expect(subject.raw_neighbor_coords(top_left)).to eq([[0, 1], [1, 0], [1, 1]])
+        expect(subject.raw_neighbor_coords(bottom_left)).to eq([[8, 0], [8, 1], [9, 1]])
+        expect(subject.raw_neighbor_coords(top_right)).to eq([[0, 8], [1, 8], [1, 9]])
+        expect(subject.raw_neighbor_coords(bottom_right)).to eq([[8, 8], [8, 9], [9, 8]])
+      end
+
+      xit "can get the neighboring values" do
+        expect(subject.neighbors(top_left)).to eq([1, 3])
+        expect(subject.neighbors(bottom_left)).to eq([8, 8])
+        expect(subject.neighbors(top_right)).to eq([1, 1])
+        expect(subject.neighbors(bottom_right)).to eq([7, 9])
+      end
+    end
+
+    context "when checking edge/border pieces" do
+      let(:left_border) { [2, 0] }
+      let(:right_border) { [3, 9] }
+      let(:top_border) { [0, 6] }
+      let(:bottom_border) { [9, 3] }
+
+      it "can get the neighboring coordinates" do
+        expect(subject.raw_neighbor_coords(left_border)).to eq([
+          [1, 0], [1, 1], [2, 1], [3, 0], [3, 1],
+        ])
+        expect(subject.raw_neighbor_coords(right_border)).to eq([
+          [2, 8], [2, 9], [3, 8], [4, 8], [4, 9],
+        ])
+        expect(subject.raw_neighbor_coords(top_border)).to eq([
+          [0, 5], [0, 7], [1, 5], [1, 6], [1, 7],
+        ])
+        expect(subject.raw_neighbor_coords(bottom_border)).to eq([
+          [8, 2], [8, 3], [8, 4], [9, 2], [9, 4],
+        ])
+      end
+
+      xit "can get the neighboring points" do
+        expect(subject.neighbors(left_border)).to eq([3, 8, 8])
+        expect(subject.neighbors(right_border)).to eq([8, 2, 8])
+        expect(subject.neighbors(top_border)).to eq([4, 2, 4])
+        expect(subject.neighbors(bottom_border)).to eq([9, 9, 5])
+      end
+    end
+
+    context "when checking pieces in the middle" do
+      let(:mid1) { [2, 3] }
+      let(:mid2) { [7, 6] }
+      it "can get the neighboring coordinates" do
+        expect(subject.raw_neighbor_coords(mid1)).to eq([
+          [1, 2], [1, 3], [1, 4], [2, 2], [2, 4], [3, 2], [3, 3], [3, 4],
+        ])
+        expect(subject.raw_neighbor_coords(mid2)).to eq([
+          [6, 5], [6, 6], [6, 7], [7, 5], [7, 7], [8, 5], [8, 6], [8, 7],
+        ])
+      end
+
+      xit "can get the neighboring points" do
+        expect(subject.neighbors(mid1)).to eq([5, 7, 7, 7])
+        expect(subject.neighbors(mid2)).to eq([7, 9, 9, 7])
+      end
+    end
+  end
+
+end
