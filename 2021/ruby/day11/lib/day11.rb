72df67679b21b142c9dd66372802b574d2db4464
Cooper Gillan <cooper.gillan@gmail.com>
2021-12-18 21:41:04 +0000

    Day 11 final cleanup
    * Remove the debugging statements
    * Derive the rows and columns using Range.new (this was extra credit)
    * Remove unnecessary tests and function
    
    It might be more elegant to go back to using the Octopus object that I
    originally had. It felt right to keep moving after finally getting part
    1 and not needing a lot of changes to get part two.

--- 2021/ruby/day11/lib/day11.rb
+++ 2021/ruby/day11/lib/day11.rb
@@ -1,5 +1,4 @@
 class Cavern
-  BANNER = "==============================="
   attr_accessor :octopus_map, :height, :width, :flashes
 
   def initialize(octopus_map)
@@ -20,74 +19,42 @@ class Cavern
 
   def flash_check(row, column, flashed)
     if @octopus_map[row][column] > 9
-      puts "About to flash for row: #{row} - column: #{column} - @flashes: #{@flashes}"
       @flashes += 1
-      puts "Flashed - @flashes: #{@flashes}"
       flashed.push([row, column])
       @octopus_map[row][column] = 0
-      puts "Got a flash for row: #{row} - column: #{column}"
-      puts "Total @flashes: #{@flashes}"
-      raw_neighbor_coords(row, column).each do |(n_row, n_col)|
-        puts "Running for neighbors: row: #{n_row} col: #{n_col} - @octopus_map[n_row][n_col]: #{@octopus_map[n_row][n_col]}"
 
-        # Increment the next one and check for flashes if it hasn't already flashed
+      # Check each neighbor and increment those that have not yet flashed
+      raw_neighbor_coords(row, column).each do |(n_row, n_col)|
         if !flashed.include?([n_row, n_col])
           @octopus_map[n_row][n_col] += 1
-        flash_check(n_row, n_col, flashed)
+          flash_check(n_row, n_col, flashed)
         end
       end
     end
   end
 
   def step
-    puts BANNER
-    puts "Beginning withstep"
-    puts BANNER
-    # step_flashes = 0
-
-    # Need to incremenet everyone first (look at the instructions!)
+    # Increment each octopus before all else (I re-read the instructions!)
     @octopus_map.each_with_index.map do |row, row_idx|
-      row.each_with_index.map do |octopus, octo_idx|
-        # require "pry"; binding.pry
-        puts "Firs tloop through - checking row_idx: #{row_idx} - octo_idx: #{octo_idx}"
-        puts "Before increment: @octopus_map[row][octo_idx]: #{@octopus_map[row_idx][octo_idx]}"
+      row.each_with_index.map do |_, octo_idx|
         @octopus_map[row_idx][octo_idx] += 1
-        puts "After increment: @octopus_map[row][octo_idx]: #{@octopus_map[row_idx][octo_idx]}"
       end
     end
 
-    # Now check for flashes
+    # Go back through to log the flashes and increment neighbors
     flashed = []
     @octopus_map.each_with_index.map do |row, row_idx|
-      row.each_with_index.map do |octopus, octo_idx|
-        puts "Second loop through - checking row_idx: #{row_idx} - octo_idx: #{octo_idx}"
-        puts "Before flash check: @flashes: #{@flashes} - flashed: #{flashed}"
+      row.each_with_index.map do |_, octo_idx|
         flash_check(row_idx, octo_idx, flashed)
       end
     end
-    puts BANNER
-    puts "Finished wthstep! @ocotopus_Map: #{@octopus_map}"
-    puts BANNER
-    return flashed
+    flashed
   end
 
   def raw_neighbor_coords(row, column)
-    # TODO: find some Ruby helper methods like each_cons that can derive neighbor coordinates
-    previous_row = row - 1
-    next_row = row + 1
-    previous_column = column - 1
-    next_column = column + 1
-
-    neighbors_check = [
-      [previous_row, previous_column],
-      [previous_row, column],
-      [previous_row, next_column],
-      [row, previous_column],
-      [row, next_column],
-      [next_row, previous_column],
-      [next_row, column],
-      [next_row, next_column],
-    ]
+    neighbors_check = Range.new(row - 1, row + 1).to_a
+      .product(Range.new(column - 1, column + 1).to_a)
+      .reject { |(r, c)| (r == row && c == column ) }
 
     neighbors_check.map do |(row_check, column_check)|
       next unless row_check.between?(0, @height - 1) && column_check.between?(0, @width - 1)
@@ -101,37 +68,23 @@ class Cavern
   end
 
   def part2
-    cavern_size = @height * @width
-
     flashed = []
     step_number = 0
+    cavern_size = @height * @width
 
     while flashed.size < cavern_size
-      puts "Step number: #{step_number}"
       flashed = step
       step_number += 1
-      if flashed.size == cavern_size
-        puts "Step number #{step_number} - cavern_size: #{cavern_size} - flashed.size: #{flashed.size}"
-        return step_number
-      end
     end
+    step_number
   end
 end
 
 
-# class Octopus
-#   attr_accessor :energy_level
-#
-#   def initialize(energy_level)
-#     @energy_level = energy_level
-#   end
-#
-# end
-
-
 if $PROGRAM_NAME  == __FILE__
-  cavern = Cavern.from_file("lib/input.txt")
+  cavern1 = Cavern.from_file("lib/input.txt")
+  puts "Answer for part 1: #{cavern1.part1}"
 
-  # puts "Answer for part 1: #{cavern.part1}"
-  puts "Answer for part 2: #{cavern.part2}"
+  cavern2 = Cavern.from_file("lib/input.txt")
+  puts "Answer for part 2: #{cavern2.part2}"
 end
--- 2021/ruby/day11/spec/day11_spec.rb
+++ 2021/ruby/day11/spec/day11_spec.rb
@@ -1,9 +1,5 @@
 require "day11"
 
-# def octo(energy_level)
-#   Octopus.new(energy_level)
-# end
-
 describe Cavern do
   subject { described_class.from_file("spec/test_input.txt") }
 
@@ -30,238 +26,120 @@ describe Cavern do
   end
 
 
-  context "given a point on the map" do
+  context "#raw_neighbors_coords" do
     context "when checking corners" do
-      let(:top_left) { [0, 0] }
-      let(:bottom_left) { [9, 0] }
-      let(:top_right) { [0, 9] }
-      let(:bottom_right) { [9, 9] }
+      let(:first) { 0 }
+      let(:last) { 9 }
 
-      it "can get the neighboring coordinates" do
-        expect(subject.raw_neighbor_coords(top_left)).to eq([[0, 1], [1, 0], [1, 1]])
-        expect(subject.raw_neighbor_coords(bottom_left)).to eq([[8, 0], [8, 1], [9, 1]])
-        expect(subject.raw_neighbor_coords(top_right)).to eq([[0, 8], [1, 8], [1, 9]])
-        expect(subject.raw_neighbor_coords(bottom_right)).to eq([[8, 8], [8, 9], [9, 8]])
-      end
-
-      xit "can get the neighboring values" do
-        expect(subject.neighbors(top_left)).to eq([1, 3])
-        expect(subject.neighbors(bottom_left)).to eq([8, 8])
-        expect(subject.neighbors(top_right)).to eq([1, 1])
-        expect(subject.neighbors(bottom_right)).to eq([7, 9])
+      it "gets the neighboring coordinates" do
+        expect(subject.raw_neighbor_coords(first, first)).to eq([[0, 1], [1, 0], [1, 1]])
+        expect(subject.raw_neighbor_coords(last, first)).to eq([[8, 0], [8, 1], [9, 1]])
+        expect(subject.raw_neighbor_coords(first, last)).to eq([[0, 8], [1, 8], [1, 9]])
+        expect(subject.raw_neighbor_coords(last, last)).to eq([[8, 8], [8, 9], [9, 8]])
       end
     end
 
     context "when checking edge/border pieces" do
-      let(:left_border) { [2, 0] }
-      let(:right_border) { [3, 9] }
-      let(:top_border) { [0, 6] }
-      let(:bottom_border) { [9, 3] }
+      let(:left_or_top) { 0 }
+      let(:right_or_bottom) { 9 }
 
       it "can get the neighboring coordinates" do
-        expect(subject.raw_neighbor_coords(left_border)).to eq([
+        expect(subject.raw_neighbor_coords(2, left_or_top)).to eq([
           [1, 0], [1, 1], [2, 1], [3, 0], [3, 1],
         ])
-        expect(subject.raw_neighbor_coords(right_border)).to eq([
+        expect(subject.raw_neighbor_coords(3, right_or_bottom)).to eq([
           [2, 8], [2, 9], [3, 8], [4, 8], [4, 9],
         ])
-        expect(subject.raw_neighbor_coords(top_border)).to eq([
+        expect(subject.raw_neighbor_coords(left_or_top, 6)).to eq([
           [0, 5], [0, 7], [1, 5], [1, 6], [1, 7],
         ])
-        expect(subject.raw_neighbor_coords(bottom_border)).to eq([
+        expect(subject.raw_neighbor_coords(right_or_bottom, 3)).to eq([
           [8, 2], [8, 3], [8, 4], [9, 2], [9, 4],
         ])
-      end
-
-      xit "can get the neighboring points" do
-        expect(subject.neighbors(left_border)).to eq([3, 8, 8])
-        expect(subject.neighbors(right_border)).to eq([8, 2, 8])
-        expect(subject.neighbors(top_border)).to eq([4, 2, 4])
-        expect(subject.neighbors(bottom_border)).to eq([9, 9, 5])
       end
     end
 
     context "when checking pieces in the middle" do
-      let(:mid1) { [2, 3] }
-      let(:mid2) { [7, 6] }
       it "can get the neighboring coordinates" do
-        expect(subject.raw_neighbor_coords(mid1)).to eq([
+        expect(subject.raw_neighbor_coords(2, 3)).to eq([
           [1, 2], [1, 3], [1, 4], [2, 2], [2, 4], [3, 2], [3, 3], [3, 4],
         ])
-        expect(subject.raw_neighbor_coords(mid2)).to eq([
+        expect(subject.raw_neighbor_coords(7, 6)).to eq([
           [6, 5], [6, 6], [6, 7], [7, 5], [7, 7], [8, 5], [8, 6], [8, 7],
         ])
-      end
-
-      xit "can get the neighboring points" do
-        expect(subject.neighbors(mid1)).to eq([5, 7, 7, 7])
-        expect(subject.neighbors(mid2)).to eq([7, 9, 9, 7])
       end
     end
   end
 
   context "#step" do
-    context "when running for a smaller example" do
-      subject { described_class.new([
-        [4, 5, 8, 5],
-        [6, 4, 5, 5],
-        [4, 1, 3, 3],
-        [5, 7, 3, 8],
-      ]) }
-      it "increments each square by one for one step" do
-        subject.step
-        expect(subject.octopus_map).to match_array([
-          [5, 6, 9, 6],
-          [7, 5, 6, 6],
-          [5, 2, 4, 4],
-          [6, 8, 4, 9],
-        ])
-      end
+    subject { described_class.from_file("spec/test_input.txt") }
 
-      it "increments each square and neighbors of flashing octopi, counts flashes" do
-        2.times { subject.step }
-        expect(subject.octopus_map).to match_array([
-          [6, 8, 0, 8],
-          [8, 7, 8, 8],
-          [6, 3, 6, 6],
-          [7, 9, 6, 0],
-        ])
-        expect(subject.flashes).to eq(2)
-      end
+    it "increments and counts flashes for 2 steps" do
+      expect(subject.octopus_map).to match_array([
+        [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
+        [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
+        [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
+        [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
+        [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
+        [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
+        [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
+        [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
+        [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
+        [5, 2, 8, 3, 7, 5, 1, 5, 2, 6],
+      ])
+      expect(subject.flashes).to eq(0)
 
-      it "increments each square and neighbors of flashing octopi, counts flashes" do
-        4.times { subject.step }
-        expect(subject.octopus_map).to match_array([
-          [0, 1, 1, 9],
-          [0, 9, 9, 9],
-          [9, 6, 8, 7],
-          [9, 0, 8, 1],
-        ])
-        expect(subject.flashes).to eq(3)
-      end
+      2.times { subject.step }
+
+      expect(subject.flashes).to eq(35)
+      expect(subject.octopus_map).to match_array([
+        [8, 8, 0, 7, 4, 7, 6, 5, 5, 5],
+        [5, 0, 8, 9, 0, 8, 7, 0, 5, 4],
+        [8, 5, 9, 7, 8, 8, 9, 6, 0, 8],
+        [8, 4, 8, 5, 7, 6, 9, 6, 0, 0],
+        [8, 7, 0, 0, 9, 0, 8, 8, 0, 0],
+        [6, 6, 0, 0, 0, 8, 8, 9, 8, 9],
+        [6, 8, 0, 0, 0, 0, 5, 9, 4, 3],
+        [0, 0, 0, 0, 0, 0, 7, 4, 5, 6],
+        [9, 0, 0, 0, 0, 0, 0, 8, 7, 6],
+        [8, 7, 0, 0, 0, 0, 6, 8, 4, 8],
+      ])
     end
 
-    context "when running for the main example" do
-      subject { described_class.from_file("spec/test_input.txt") }
+    it "increments and counts flashes for 10 steps" do
+      10.times { subject.step }
 
-      it "increments and counts flashes for 2 steps" do
-        expect(subject.octopus_map).to match_array([
-          [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
-          [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
-          [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
-          [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
-          [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
-          [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
-          [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
-          [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
-          [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
-          [5, 2, 8, 3, 7, 5, 1, 5, 2, 6],
-        ])
-        expect(subject.flashes).to eq(0)
+      expect(subject.flashes).to eq(204)
+      expect(subject.octopus_map).to match_array([
+        [0, 4, 8, 1, 1, 1, 2, 9, 7, 6],
+        [0, 0, 3, 1, 1, 1, 2, 0, 0, 9],
+        [0, 0, 4, 1, 1, 1, 2, 5, 0, 4],
+        [0, 0, 8, 1, 1, 1, 1, 4, 0, 6],
+        [0, 0, 9, 9, 1, 1, 1, 3, 0, 6],
+        [0, 0, 9, 3, 5, 1, 1, 2, 3, 3],
+        [0, 4, 4, 2, 3, 6, 1, 1, 3, 0],
+        [5, 5, 3, 2, 2, 5, 2, 3, 5, 0],
+        [0, 5, 3, 2, 2, 5, 0, 6, 0, 0],
+        [0, 0, 3, 2, 2, 4, 0, 0, 0, 0],
+      ])
+    end
 
-        subject.step
+    it "increments and counts flashes for the 100 steps in the instructions" do
+      100.times { subject.step }
 
-        expect(subject.flashes).to eq(0)
-        expect(subject.octopus_map).to match_array([
-          [6, 5, 9, 4, 2, 5, 4, 3, 3, 4],
-          [3, 8, 5, 6, 9, 6, 5, 8, 2, 2],
-          [6, 3, 7, 5, 6, 6, 7, 2, 8, 4],
-          [7, 2, 5, 2, 4, 4, 7, 2, 5, 7],
-          [7, 4, 6, 8, 4, 9, 6, 5, 8, 9],
-          [5, 2, 7, 8, 6, 3, 5, 7, 5, 6],
-          [3, 2, 8, 7, 9, 5, 2, 8, 3, 2],
-          [7, 9, 9, 3, 9, 9, 2, 2, 4, 5],
-          [5, 9, 5, 7, 9, 5, 9, 6, 6, 5],
-          [6, 3, 9, 4, 8, 6, 2, 6, 3, 7],
-        ])
-
-        subject.step
-        expect(subject.flashes).to eq(35)
-        expect(subject.octopus_map).to match_array([
-          [8, 8, 0, 7, 4, 7, 6, 5, 5, 5],
-          [5, 0, 8, 9, 0, 8, 7, 0, 5, 4],
-          [8, 5, 9, 7, 8, 8, 9, 6, 0, 8],
-          [8, 4, 8, 5, 7, 6, 9, 6, 0, 0],
-          [8, 7, 0, 0, 9, 0, 8, 8, 0, 0],
-          [6, 6, 0, 0, 0, 8, 8, 9, 8, 9],
-          [6, 8, 0, 0, 0, 0, 5, 9, 4, 3],
-          [0, 0, 0, 0, 0, 0, 7, 4, 5, 6],
-          [9, 0, 0, 0, 0, 0, 0, 8, 7, 6],
-          [8, 7, 0, 0, 0, 0, 6, 8, 4, 8],
-        ])
-      end
-
-      it "increments and counts flashes for 10 steps" do
-        expect(subject.octopus_map).to match_array([
-          [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
-          [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
-          [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
-          [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
-          [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
-          [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
-          [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
-          [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
-          [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
-          [5, 2, 8, 3, 7, 5, 1, 5, 2, 6],
-        ])
-        expect(subject.flashes).to eq(0)
-
-        10.times { subject.step }
-
-        expect(subject.flashes).to eq(204)
-        expect(subject.octopus_map).to match_array([
-          [0, 4, 8, 1, 1, 1, 2, 9, 7, 6],
-          [0, 0, 3, 1, 1, 1, 2, 0, 0, 9],
-          [0, 0, 4, 1, 1, 1, 2, 5, 0, 4],
-          [0, 0, 8, 1, 1, 1, 1, 4, 0, 6],
-          [0, 0, 9, 9, 1, 1, 1, 3, 0, 6],
-          [0, 0, 9, 3, 5, 1, 1, 2, 3, 3],
-          [0, 4, 4, 2, 3, 6, 1, 1, 3, 0],
-          [5, 5, 3, 2, 2, 5, 2, 3, 5, 0],
-          [0, 5, 3, 2, 2, 5, 0, 6, 0, 0],
-          [0, 0, 3, 2, 2, 4, 0, 0, 0, 0],
-        ])
-      end
-
-      it "increments and counts flashes for the 100 steps in the instructions" do
-        100.times { subject.step }
-
-        expect(subject.flashes).to eq(1656)
-        expect(subject.octopus_map).to match_array([
-          [0, 3, 9, 7, 6, 6, 6, 8, 6, 6],
-          [0, 7, 4, 9, 7, 6, 6, 9, 1, 8],
-          [0, 0, 5, 3, 9, 7, 6, 9, 3, 3],
-          [0, 0, 0, 4, 2, 9, 7, 8, 2, 2],
-          [0, 0, 0, 4, 2, 2, 9, 8, 9, 2],
-          [0, 0, 5, 3, 2, 2, 2, 8, 7, 7],
-          [0, 5, 3, 2, 2, 2, 2, 9, 6, 6],
-          [9, 3, 2, 2, 2, 2, 8, 9, 6, 6],
-          [7, 9, 2, 2, 2, 8, 6, 8, 6, 6],
-          [6, 7, 8, 9, 9, 9, 8, 7, 6, 6],
-        ])
-      end
+      expect(subject.flashes).to eq(1656)
+      expect(subject.octopus_map).to match_array([
+        [0, 3, 9, 7, 6, 6, 6, 8, 6, 6],
+        [0, 7, 4, 9, 7, 6, 6, 9, 1, 8],
+        [0, 0, 5, 3, 9, 7, 6, 9, 3, 3],
+        [0, 0, 0, 4, 2, 9, 7, 8, 2, 2],
+        [0, 0, 0, 4, 2, 2, 9, 8, 9, 2],
+        [0, 0, 5, 3, 2, 2, 2, 8, 7, 7],
+        [0, 5, 3, 2, 2, 2, 2, 9, 6, 6],
+        [9, 3, 2, 2, 2, 2, 8, 9, 6, 6],
+        [7, 9, 2, 2, 2, 8, 6, 8, 6, 6],
+        [6, 7, 8, 9, 9, 9, 8, 7, 6, 6],
+      ])
     end
   end
 end
-
-# describe Octopus do
-#   subject { described_class.new(6) }
-#   context "#increment_and_flash" do
-#     context "when below energy level 9" do
-#       it "increments its energy level by 1 when below 9 and does not flash" do
-#         expect(subject.energy_level).to eq(6)
-#         expect(subject.increment_and_flash).to be(nil)
-#         expect(subject.energy_level).to eq(7)
-#       end
-#     end
-#
-#     context "when at energy level 9" do
-#       subject { described_class.new(9) }
-#       it "goes to 0 energy level when incremented from 9 and returns one flash" do
-#         expect(subject.energy_level).to eq(9)
-#         expect(subject.increment_and_flash).to eq(1)
-#         expect(subject.energy_level).to eq(0)
-#       end
-#     end
-#   end
-# end
