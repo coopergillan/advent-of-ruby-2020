9c2518ecdee3e56bbd2253df3d4dadb12853a855
Cooper Gillan <cooper.gillan@gmail.com>
2022-03-15 16:47:05 +0000

    WIP part two - making a little progress

--- ruby/2020/day7/lib/day7.rb
+++ ruby/2020/day7/lib/day7.rb
@@ -1,63 +1,77 @@
-# class BagChecker
-#   def self.from_file(filepath)
-#     new(
-#       File.read(filepath, chomp: true).split(/\n\n/).map do |line|
-#         line.chomp.split(/\n/).flatten
-#       end
-#      )
-#   end
-# end
-
-# config = {
-#   "light red": {"bright white": 1, "muted yellow": 2},
-#   "dark orange": {"bright white": 3, "muted yellow": 4},
-#   "bright white": {"shiny gold": 1},
-#   "muted yellow": {"shiny gold": 2, "faded blue": 9},
-# }
-
-class BagDescription
-  attr_accessor :details
+class BagInfo
+  attr_accessor :type, :capacity
 
   def self.from_raw(raw_input)
-    matcher = raw_input.match(/^(?<bag_type>\w+\s{1}\w+)\sbags contain\s(?<raw_capacity>.*)\.$/)
-    if matcher
-      bag_type = matcher[:bag_type].to_s
-      puts "Check bag_type: #{bag_type}"
-      raw_capacity = matcher[:raw_capacity].to_s
-      puts "raw_capacity: #{raw_capacity}"
+    type, raw_capacity = raw_input.split(" bags contain ")
+    raw_capacity = raw_capacity.chomp(".")
+    puts "type: #{type} - raw_capacity: #{raw_capacity}"
+
+    new(type, raw_capacity)
+  end
+
+  def initialize(type, capacity)
+    @type = type
+    @capacity = capacity
+  end
+
+  def parse_capacity(raw_input)
+    capacity_hash = {}
+    return capacity_hash if raw_input == "no other bags"
+
+    puts "getting ready to tap raw_input #{raw_input}"
+    # {}.tap do |hash|
+    raw_input.split(",").each do |raw_bag|
+      puts "Checking raw_bag: #{raw_bag}"
+      bag_regex = /^(?<bag_count>\d{1}) (?<bag_type>\w+\s{1}\w+) bag(s)?$/
+      bag_matcher = raw_bag.match(bag_regex)
+      puts "bag_matcher: #{bag_matcher}"
+      if bag_matcher
+        puts "did we make it to building the hash?"
+        capacity_hash[bag_matcher[:bag_type].to_s] = bag_matcher[:bag_count].to_i
+      end
     end
-    if raw_capacity == "no other bags"
-      capacity = nil
-    else
-      capacity_matcher = raw_capacity.match(/^(?<part1>.*)\sbag(s)?((,\s{1})?(?<part2>.*)\sbag(s)?)?$/)
-      puts "capacity_matcher: #{capacity_matcher}"
+    capacity_hash
+  end
 
-      raw_part1 = capacity_matcher[:part1].to_s
-      raw_part2 = capacity_matcher[:part2].to_s
-
-      puts "raw_part1: #{raw_part1}"
-      puts "raw_part2: #{raw_part2}"
-      capacity = {}
-
-      bag1_regex = /(?<count1>\d{1})\s(?<type1>\w+\s{1}\w+)/
-      bag1_matcher = raw_part1.match(bag1_regex)
-      puts "bag1_matcher: #{bag1_matcher}"
-
-      if bag1_matcher
-        if bag1_matcher[:count1] && bag1_matcher[:type1]
-          capacity.merge!({bag1_matcher[:type1] => bag1_matcher[:count1].to_i})
-        end
-      end
-
-      bag2_regex = /(?<count2>\d{1})\s(?<type2>\w+\s{1}\w+)/
-      bag2_matcher = raw_part2.match(bag2_regex)
-      puts "bag2_matcher: #{bag2_matcher}"
-
-      if bag2_matcher
-        if bag2_matcher[:count2] && bag2_matcher[:type2]
-          capacity.merge!({bag2_matcher[:type2] => bag2_matcher[:count2].to_i})
-        end
-      end
+    # matcher = raw_input.match(/^(?<bag_type>\w+\s{1}\w+)\sbags contain\s(?<raw_capacity>.*)\.$/)
+    # if matcher
+    #   bag_type = matcher[:bag_type].to_s
+    #   puts "Check bag_type: #{bag_type}"
+    #   raw_capacity = matcher[:raw_capacity].to_s
+    #   puts "raw_capacity: #{raw_capacity}"
+    # end
+    # if raw_capacity == "no other bags"
+    #   capacity = nil
+    # else
+    #   capacity_matcher = raw_capacity.match(/^(?<part1>.*)\sbag(s)?((,\s{1})?(?<part2>.*)\sbag(s)?)?$/)
+    #   puts "capacity_matcher: #{capacity_matcher}"
+    #
+    #   raw_part1 = capacity_matcher[:part1].to_s
+    #   raw_part2 = capacity_matcher[:part2].to_s
+    #
+    #   puts "raw_part1: #{raw_part1}"
+    #   puts "raw_part2: #{raw_part2}"
+    #   capacity = {}
+    #
+    #   bag1_regex = /(?<count1>\d{1})\s(?<type1>\w+\s{1}\w+)/
+    #   bag1_matcher = raw_part1.match(bag1_regex)
+    #   puts "bag1_matcher: #{bag1_matcher}"
+    #
+    #   if bag1_matcher
+    #     if bag1_matcher[:count1] && bag1_matcher[:type1]
+    #       capacity.merge!({bag1_matcher[:type1] => bag1_matcher[:count1].to_i})
+    #     end
+    #   end
+    #
+    #   bag2_regex = /(?<count2>\d{1})\s(?<type2>\w+\s{1}\w+)/
+    #   bag2_matcher = raw_part2.match(bag2_regex)
+    #   puts "bag2_matcher: #{bag2_matcher}"
+    #
+    #   if bag2_matcher
+    #     if bag2_matcher[:count2] && bag2_matcher[:type2]
+    #       capacity.merge!({bag2_matcher[:type2] => bag2_matcher[:count2].to_i})
+    #     end
+    #   end
 
       # capacity_regex = /(?<count1>\d{1})\s(?<type1>\w+\s{1}\w+)\sbag(s)?((,\s{1})?(?<count2>\d{1})(?<type2>\w+\s{1}\w+)\sbag(s)?)?/
       # capacity_matcher = raw_capacity.match(capacity_regex)
@@ -71,17 +85,13 @@ class BagDescription
       #     capacity.merge!({capacity_matcher[:type2] => capacity_matcher[:count2].to_i})
       #   end
       # end
-    end
-
-    puts "using capacity: #{capacity}"
-    puts "finished bag_tupe: #{bag_type}"
-    puts "#################"
-    BagDescription.new({bag_type => capacity})
-  end
-
-  def initialize(details)
-    @details = details
-  end
+  #   end
+  #
+  #   puts "using capacity: #{capacity}"
+  #   puts "finished bag_tupe: #{bag_type}"
+  #   puts "#################"
+  #   BagDescription.new({bag_type => capacity})
+  # end
 end
 
 
@@ -109,3 +119,21 @@ if $PROGRAM_NAME  == __FILE__
   # part2_answer = question_log.part2
   # puts "Got part2_answer: #{part2_answer}"
 end
+#
+# class BagChecker
+#   def self.from_file(filepath)
+#     new(
+#       File.read(filepath, chomp: true).split(/\n\n/).map do |line|
+#         line.chomp.split(/\n/).flatten
+#       end
+#      )
+#   end
+# end
+
+# config = {
+#   "light red": {"bright white": 1, "muted yellow": 2},
+#   "dark orange": {"bright white": 3, "muted yellow": 4},
+#   "bright white": {"shiny gold": 1},
+#   "muted yellow": {"shiny gold": 2, "faded blue": 9},
+# }
+
--- ruby/2020/day7/spec/day7_spec.rb
+++ ruby/2020/day7/spec/day7_spec.rb
@@ -1,34 +1,53 @@
 require "day7"
   # subject { described_class.from_file("spec/test_input.txt") }
 
-describe BagDescription do
+describe BagInfo do
   context "#from_raw" do
     it "creates instances with no capacity" do
-      bag_description = described_class.from_raw(
+      bag_info = described_class.from_raw(
         "dotted black bags contain no other bags.",
       )
-      expect(bag_description.details).to include(
-        {"dotted black" => nil},
-      )
+      expect(bag_info.type).to eq("dotted black")
+      expect(bag_info.capacity).to eq({})
     end
 
-    it "creates instances with a capacity of one type of bag" do
-      bag_description = described_class.from_raw(
-        "bright white bags contain 1 shiny gold bag.",
-      )
-      expect(bag_description.details).to include(
-        {"bright white" => {"shiny gold" => 1}},
-      )
+  context "#parse_capacity" do
+    let(:raw_input) { "dotted black bags contain no other bags" }
+    subject { described_class.from_raw(raw_input) }
+
+    it "can convert bags with no capacity to an empty hash" do
+      expect(subject.parse_capacity("no other bags")).to eq({})
     end
 
-    it "creates instances with a capacity of two types of bags" do
-      bag_description = described_class.from_raw(
-        "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
-      )
-      expect(bag_description.details).to include({
-        "dark orange" => {"bright white" => 3, "muted yellow" => 4}
-      })
+    it "can convert bags with one capacity specification into a one-element hash" do
+      expect(subject.parse_capacity("1 shiny gold bag")).to eq({"shiny gold" => 1})
     end
+
+    it "can convert bags with two capacity specifications into a two-element hash" do
+      expect(subject.parse_capacity("3 faded blue bags, 4 dotted black bags")).to include(
+        {"faded blue" => 3, "dotted black" => 4},
+      )
+    end
+  end
+
+  #   it "creates instances with a capacity of one type of bag" do
+  #     bag_description = described_class.from_raw(
+  #       "bright white bags contain 1 shiny gold bag.",
+  #     )
+  #     expect(bag_description.details).to include(
+  #       {"bright white" => {"shiny gold" => 1}},
+  #     )
+  #   end
+  #
+  #   it "creates instances with a capacity of two types of bags" do
+  #     bag_description = described_class.from_raw(
+  #       "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+  #     )
+  #     expect(bag_description.details).to include({
+  #       "dark orange" => {"bright white" => 3, "muted yellow" => 4}
+  #     })
+  #   end
+
   end
 end
 
