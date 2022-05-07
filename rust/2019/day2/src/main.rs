d3892852d52b09c7f398150cd18d438c185ae21d
Cooper Gillan <cooper.gillan@gmail.com>
2022-05-07 19:04:49 +0000

    WIP working on better Intcode logic
    Holy cow this is a challenge with Rust. Gonna keep picking at it.

--- rust/2019/day2/src/main.rs
+++ rust/2019/day2/src/main.rs
@@ -7,6 +7,11 @@ use std::fs;
 //     raw_input: vec<usize>,
 // }
 
+#[derive(Debug)]
+struct IntcodeInput {
+    code_sequence: Vec<usize>,
+}
+
 #[derive(Debug, PartialEq)]
 struct Intcode {
     op_code: usize,
@@ -15,33 +20,52 @@ struct Intcode {
 }
 
 impl Intcode {
-    fn process_input(&self) -> Vec<usize> {
-        let mut value_and_location = vec![];
+    fn process_input(&self, all_input: &mut Vec<usize>) {
+        // let mut value_and_location = vec![];
 
-        if let [val1, val2] = &self.operand_positions[..] {
-            if self.op_code == 1 {
-                let value = val1 + val2;
-                value_and_location.push(value);
-                value_and_location.push(self.pos_to_write)
-            }
+        if let [pos1, pos2] = &self.operand_positions[..] {
+            match self.op_code {
+                1 => {
+                    let value = all_input[*pos1] + all_input[*pos2];
+                    all_input[self.pos_to_write] = value;
+                    // value_and_location.push(value);
+                    // value_and_location.push(self.pos_to_write)
+                }
+                _ => println!("Couldn't find op_code"),
+            };
         }
-        value_and_location
+
+        // Now go write the computed value to the correct spot
+        // value_and_locatio\vp
     }
 }
 
 fn read_input(file_path: &str) -> Vec<usize> {
-    let raw = fs::read_to_string(file_path)
+    fs::read_to_string(file_path)
         .expect("Unable to read file")
-        .replace("\n", "");
-
-    let contents: Vec<usize> = raw
+        .replace("\n", "")
         .split(",")
         .map(|v| v.parse::<usize>().expect("Unable to parse"))
-        .collect();
-    contents
+        .collect()
 }
 
-fn main() {}
+fn main() {
+    let code_sequence = read_input("test_input.txt");
+    let mut full_input = IntcodeInput { code_sequence };
+
+    assert_eq!(full_input.code_sequence.len(), 12);
+
+    let chunkable = full_input.code_sequence.clone();
+    for intcode_raw in chunkable.chunks(4) {
+    // for intcode_raw in full_input.code_sequence.chunks(4) {
+        let intcode = Intcode {
+            op_code: intcode_raw[0],
+            operand_positions: vec![intcode_raw[1],intcode_raw[2]],
+            pos_to_write: intcode_raw[3],
+        };
+        intcode.process_input(&mut full_input.code_sequence);
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -57,23 +81,30 @@ mod tests {
 
     #[test]
     fn test_process_intcode_sum_input() {
+        let mut all_input = vec![1, 2, 5, 6, 7, 7];
+
         let intcode1 = Intcode {
             op_code: 1,
-            operand_positions: vec![10, 20],
-            pos_to_write: 30,
+            operand_positions: vec![4, 1],
+            pos_to_write: 5,
         };
+        intcode1.process_input(&mut all_input);
 
-        assert_eq!(intcode1.process_input(), vec![30, 30],);
+        assert_eq!(vec![1, 2, 5, 6, 7, 9], all_input);
     }
 
     #[test]
+    #[ignore]
     fn test_process_intcode_multiply_input() {
+        let mut all_input = vec![1, 2, 5, 6, 7, 7];
         let intcode1 = Intcode {
             op_code: 2,
             operand_positions: vec![3, 11],
             pos_to_write: 0,
         };
 
-        assert_eq!(intcode1.process_input(), vec![30, 30],);
+        intcode1.process_input(&mut all_input);
+
+        assert_eq!(vec![3, 1, 4], all_input);
     }
 }
