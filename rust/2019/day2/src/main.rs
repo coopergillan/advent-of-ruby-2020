111357d1ad230cd6ea3520c61666bbe2ff4039a1
Cooper Gillan <cooper.gillan@gmail.com>
2022-05-20 17:07:35 +0000

    Last part one refactor
    I like where this ended up with using the mutable Input value alongside
    the instructions that can handle some of the logic of processing each
    one.

--- rust/2019/day2/src/main.rs
+++ rust/2019/day2/src/main.rs
@@ -4,59 +4,6 @@ use std::fs;
 
 const INSTRUCTION_SIZE: usize = 4;
 
-#[derive(Debug, PartialEq)]
-struct Intcode {
-    op_code: usize,
-    operand_positions: Vec<usize>,
-    pos_to_write: usize,
-}
-
-impl Intcode {
-    fn process_input(&self, all_input: &mut Vec<usize>) {
-        // let mut value_and_location = vec![];
-
-        if let [pos1, pos2] = &self.operand_positions[..] {
-            match self.op_code {
-                1 => {
-                    let value = all_input[*pos1] + all_input[*pos2];
-                    all_input[self.pos_to_write] = value;
-                    // value_and_location.push(value);
-                    // value_and_location.push(self.pos_to_write)
-                }
-                _ => println!("Couldn't find op_code"),
-            };
-        }
-
-        // Now go write the computed value to the correct spot
-        // value_and_locatio\vp
-    }
-}
-
-struct Input {
-    input_file: String,
-    noun: usize,
-    verb: usize,
-}
-
-impl Input {
-    fn new(input_file_name: &str, noun: usize, verb: usize) -> Self {
-        let input_file = input_file_name.to_string();
-
-        Input {
-            input_file,
-            noun,
-            verb,
-        }
-    }
-
-    fn prepare(&self) -> Vec<usize> {
-        let mut raw_input = read_input_file(&self.input_file);
-        raw_input[1] = self.noun;
-        raw_input[2] = self.verb;
-        raw_input
-    }
-}
-
 fn read_input_file(file_path: &str) -> Vec<usize> {
     fs::read_to_string(file_path)
         .expect("Unable to read file")
@@ -66,38 +13,80 @@ fn read_input_file(file_path: &str) -> Vec<usize> {
         .collect()
 }
 
+struct Input {
+    details: Vec<usize>,
+}
+
+impl Input {
+    /// Read file contents in, then update noun and verb
+    fn new(input_file_name: &str, noun: usize, verb: usize) -> Self {
+        let mut details = read_input_file(input_file_name);
+        details[1] = noun;
+        details[2] = verb;
+
+        Input { details }
+    }
+
+    fn process_instruction(&mut self, instruction: Instruction) {
+        let value1 = self.details[instruction.param1];
+        let value2 = self.details[instruction.param2];
+
+        let value = match instruction.opcode {
+            1 => value1 + value2,
+            2 => value1 * value2,
+            _ => {
+                println!("Couldn't find opcode: {:?}\n", instruction.opcode);
+                0
+            }
+        };
+        if value > 0 {
+            self.details[instruction.param3] = value;
+        };
+    }
+
+    fn size(&self) -> usize {
+        self.details.len()
+    }
+}
+
+struct Instruction {
+    opcode: usize,
+    param1: usize,
+    param2: usize,
+    param3: usize,
+}
+
+impl Instruction {
+    fn new(opcode: usize, param1: usize, param2: usize, param3: usize) -> Self {
+        Instruction {
+            opcode,
+            param1,
+            param2,
+            param3,
+        }
+    }
+}
+
 fn main() {
     let noun = 12;
     let verb = 2;
-    let raw_input = Input::new("input.txt", noun, verb);
-    let mut full_input = raw_input.prepare(); // code_sequence.clone();
+    let mut input = Input::new("input.txt", noun, verb);
 
     // Iterate through the values four at a time using indexes
-    let input_size = full_input.len();
-    for intcode_raw in (0..input_size).step_by(INSTRUCTION_SIZE) {
+    for intcode_raw in (0..input.size()).step_by(INSTRUCTION_SIZE) {
         let instruction_end = intcode_raw + INSTRUCTION_SIZE;
 
-        if let [opcode, param1, param2, param3] = full_input[intcode_raw..instruction_end] {
-            let value1 = full_input[param1];
-            let value2 = full_input[param2];
-
-            let value = match opcode {
-                1 => value1 + value2,
-                2 => value1 * value2,
+        if let [opcode, param1, param2, param3] = input.details[intcode_raw..instruction_end] {
+            let instruction = Instruction::new(opcode, param1, param2, param3);
+            match instruction.opcode {
                 99 => break,
-                _ => {
-                    println!("Couldn't find opcode: {:?}\n", opcode);
-                    0
-                }
-            };
-            if value > 0 {
-                full_input[param3] = value;
-            };
+                _ => input.process_instruction(instruction),
+            }
         }
     }
     println!(
         "Finished iterating - zeroth element of full_input: {:?}",
-        full_input[0]
+        input.details[0]
     );
 }
 
@@ -105,23 +94,40 @@ fn main() {
 mod tests {
     use super::*;
 
+    const INPUT_FILE_NAME: &str = "test_input.txt";
+    const NOUN: usize = 11;
+    const VERB: usize = 3;
+
+    fn input_for_testing() -> Input {
+        Input::new(INPUT_FILE_NAME, NOUN, VERB)
+    }
+
     #[test]
     fn test_read_input_file() {
         assert_eq!(
-            read_input_file("test_input.txt"),
+            read_input_file(INPUT_FILE_NAME),
             vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
         );
     }
 
     #[test]
     fn test_prepare_input() {
-        let noun = 12;
-        let verb = 2;
-        let input = Input::new("test_input.txt", noun, verb);
+        let input = input_for_testing();
+        assert_eq!(
+            input.details,
+            vec![1, 11, 3, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+        );
+    }
+
+    #[test]
+    fn test_process_instruction() {
+        let mut input = input_for_testing();
+        let instruction = Instruction::new(1, 11, 3, 3);
+        input.process_instruction(instruction);
 
         assert_eq!(
-            input.prepare(),
-            vec![1, 12, 2, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+            input.details,
+            vec![1, 11, 3, 53, 2, 3, 11, 0, 99, 30, 40, 50],
         );
     }
 }
