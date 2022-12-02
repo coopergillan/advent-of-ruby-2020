e963bd3166e316abf50c7ab96555ced2a31ab8ca
Cooper Gillan <cooper.gillan@gmail.com>
2022-12-02 22:48:41 +0000

    Finish with double line break struct example
    Create a struct with a from_file method that can work when there are
    double line breaks distinguishing classes. This is similar to day one of
    2022.

--- rust/template/src/main.rs
+++ rust/template/src/main.rs
@@ -58,38 +58,67 @@ impl TopLevelStructMultipleIntsPerLine {
             })
             .collect();
 
-        println!("Creating struct with processed contents: {:?}", processed_contents);
+        println!(
+            "Creating struct with processed contents: {:?}",
+            processed_contents
+        );
         Self::new(processed_contents)
     }
 }
 
-// impl
-// For graph type of problem use vector of vectors
-// fn new(input_data: Vec<Vec<isize>>) -> Self {
+#[derive(Debug, PartialEq)]
+struct TopLevelStructDoubleLineBreak {
+    // Double line breaks with integers on each line for data
+    input_data: Vec<Vec<usize>>,
+}
 
-// TODO: implement one where double line breaks distinguish other objects - elves from 2022 or Bingo boards from 2022
-// For reading input with double line breaks that distinguish separate objects
-// let raw_content_objects = raw_content.split("\n\n");
+impl TopLevelStructDoubleLineBreak {
+    // For one value per line use a vector of integers
+    // If negative numbers needed, change to usize
+    fn new(input_data: Vec<Vec<usize>>) -> Self {
+        TopLevelStructDoubleLineBreak { input_data }
+    }
 
-// fn read_input(file_path: &str) -> Vec<Vec<isize>> {
-//     let raw_contents = fs::read_to_string(file_path).expect("Unable to read file");
-//     let contents = raw_contents.lines();
-//     contents
-//         .map(|line| {
-//             line.to_string()
-//                 .parse::<i64>()
-//                 .expect("Unable to parse string to integer")
-//         })
-//         .collect()
-// }
+    fn from_file(file_path: &str) -> Self {
+        // For reading each line into an array
+        let raw_content = fs::read_to_string(file_path).expect("Unable to read file");
+
+        let processed_contents: Vec<Vec<usize>> = raw_content
+            .split("\n\n")
+            .map(|line| {
+                line.lines()
+                    .map(|v| v.parse::<usize>().expect("Unable to parse"))
+                    .collect()
+            })
+            .collect();
+        Self::new(processed_contents)
+    }
+
+    fn solve_part1(&self) -> usize {
+        5
+    }
+
+    fn solve_part2(&self) -> usize {
+        7
+    }
+}
+
+// Create scaffolding for more detailed struct for when
+// input needs its own definition for better code structure
+
+struct ParticularStruct {
+    input1: String,
+    input2: usize,
+}
 
 fn main() {
     println!("Hello world");
-    let input_file = "test_input_multiple_ints_per_line.txt";
+    let input_file = "test_double_line_break_input.txt";
 
-    let top_level = TopLevelStructMultipleIntsPerLine::from_file(input_file);
-    println!("Part 1 answer: {}", top_level.part1(input_file));
-    // println!("Part 2 answer: {}", part2(input_file));
+    let top_level = TopLevelStructDoubleLineBreak::from_file(input_file);
+
+    println!("Part 1 answer: {}", top_level.solve_part1());
+    println!("Part 2 answer: {}", top_level.solve_part2());
 }
 
 #[cfg(test)]
@@ -108,49 +137,42 @@ mod tests {
         let input_file = "test_input_multiple_ints_per_line.txt";
         let top_level = TopLevelStructMultipleIntsPerLine::from_file(input_file);
         println!("top_level_struct.input_data: {:?}", top_level.input_data);
-        assert_eq!(top_level.input_data, vec![
-            vec![5, 7, 13, 2],
-            vec![9,314,2718,17],
-            vec![13,8,7,4],
-            vec![4,22,19,33],
-            vec![5,20,79,13],
-        ])
+        assert_eq!(
+            top_level.input_data,
+            vec![
+                vec![5, 7, 13, 2],
+                vec![9, 314, 2718, 17],
+                vec![13, 8, 7, 4],
+                vec![4, 22, 19, 33],
+                vec![5, 20, 79, 13],
+            ]
+        )
     }
 
-    // #[test]
-    // fn test_read_input() {
-    //     assert_eq!(read_input("test_input.txt"), vec![12, 14, 1969, 100756])
-    // }
-    //
-    // #[test]
-    // fn test_part1() {
-    //     let input_file = "test_input.txt";
-    //     assert_eq!(part1(input_file), 34241);
-    // }
-    //
-    // #[test]
-    // fn test_part2() {
-    //     let input_file = "test_input.txt";
-    //     assert_eq!(part2(input_file), 51316);
-    // }
-}
+    #[test]
+    fn test_from_file_top_level_double_line_breaks() {
+        let input_file = "test_double_line_break_input.txt";
+        let top_level = TopLevelStructDoubleLineBreak::from_file(input_file);
+        println!("top_level_struct.input_data: {:?}", top_level.input_data);
+        assert_eq!(
+            top_level.input_data,
+            vec![
+                vec![1000, 2000, 3000],
+                vec![4000],
+                vec![5000, 6000],
+                vec![7000, 8000, 9000],
+                vec![10000],
+            ]
+        )
+    }
 
-// fn part1(file_path: &str) -> i64 {
-//     let input_masses = read_input(file_path);
-//     let modules = collect_modules(input_masses);
-//
-//     modules
-//         .iter()
-//         .map(|module| module.calculate_fuel_part1())
-//         .sum()
-// }
-//
-// fn part2(file_path: &str) -> i64 {
-//     let input_masses = read_input(file_path);
-//     let modules = collect_modules(input_masses);
-//
-//     modules
-//         .iter()
-//         .map(|module| module.calculate_fuel_part2())
-//         .sum()
-// }
+    #[test]
+    fn test_part1() {
+        assert_eq!(5, 6);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(22, 10);
+    }
+}
