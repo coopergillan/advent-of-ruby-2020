a8edc270b0ee5c176903094443358a403db2fdc1
Cooper Gillan <cooper.gillan@gmail.com>
2022-12-04 04:16:44 +0000

    Ruby 2022 Day three part two done
    Took longer than hoped with a pesky bug hiding in the `solve_part2`
    method. Took some time to decide how the input for the new
    `RucksackGroup` would work, but it finally came together.

--- ruby/2022/day3/lib/solver.rb
+++ ruby/2022/day3/lib/solver.rb
@@ -1,4 +1,5 @@
 class RucksackList
+  ELF_GROUP_SIZE = 3
 
   attr_accessor :input_data
 
@@ -12,39 +13,71 @@ class RucksackList
 
   def solve_part1
     input_data.reduce(0) do |total_priority, raw_rucksack|
-      total_priority + Rucksack.from_raw(raw_rucksack).priority
+      total_priority + Rucksack.new(raw_rucksack).priority
+    end
+  end
+
+  def solve_part2
+    input_data.each_slice(ELF_GROUP_SIZE).reduce(0) do |total_priority, elf_group|
+      rucksack_group = RucksackGroup.from_raw(elf_group)
+      total_priority + rucksack_group.priority
     end
   end
 end
 
-class Rucksack
-
+module RucksackCommon
   ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  attr_accessor :compartment1, :compartment2
-
-  def initialize(compartment1, compartment2)
-    @compartment1 = compartment1
-    @compartment2 = compartment2
-  end
-
-  def self.from_raw(raw_input)
-    compartment_size = raw_input.size / 2
-    compartment1, compartment2 = raw_input.chars.each_slice(compartment_size).map(&:join)
-    new(compartment1, compartment2)
-  end
-
-  def common_element
-    compartment1.chars.intersection(compartment2.chars).first
-  end
 
   def priority
     ALPHABET.index(common_element) + 1
   end
 end
 
+class Rucksack
+  include RucksackCommon
+
+  attr_accessor :contents, :compartment1, :compartment2
+
+  def initialize(contents)
+    @contents = contents
+    compartment_size = contents.size / 2
+    @compartment1, @compartment2 = contents.chars.each_slice(compartment_size).map(&:join)
+  end
+
+  def common_element
+    compartment1.chars.intersection(compartment2.chars).first
+  end
+end
+
+class RucksackGroup
+  include RucksackCommon
+
+  attr_accessor :rucksack1, :rucksack2, :rucksack3
+
+  def initialize(rucksacks)
+    @rucksack1, @rucksack2, @rucksack3 = rucksacks
+  end
+
+  def self.from_raw(raw_input)
+    input1, input2, input3 = raw_input
+    new([Rucksack.new(input1), Rucksack.new(input2), Rucksack.new(input3)])
+  end
+
+  def common_element
+    common_elements = rucksack1.contents.chars &
+      rucksack2.contents.chars &
+      rucksack3.contents.chars
+    common_elements.first
+  end
+end
+
+
 if $PROGRAM_NAME  == __FILE__
   rucksack_list = RucksackList.from_file("lib/input.txt")
 
   part1 = rucksack_list.solve_part1
   puts "Part one answer: #{part1}"
+
+  part2 = rucksack_list.solve_part2
+  puts "Part two answer: #{part2}"
 end
--- ruby/2022/day3/spec/solver_spec.rb
+++ ruby/2022/day3/spec/solver_spec.rb
@@ -21,12 +21,18 @@ describe RucksackList do
       expect(subject.solve_part1).to eq(157)
     end
   end
+
+  context "#solve_part2" do
+    it "solves part two" do
+      expect(subject.solve_part2).to eq(70)
+    end
+  end
 end
 
 describe Rucksack do
   context "#from_raw" do
     let(:raw_input) { "vJrwpWtwJgWrhcsFMMfFFhFp" }
-    subject { described_class.from_raw(raw_input) }
+    subject { described_class.new(raw_input) }
 
     it "translates the raw input into the compartments" do
       expect(subject.compartment1).to eq("vJrwpWtwJgWr")
@@ -40,5 +46,21 @@ describe Rucksack do
     it "finds the priority for the common element (a-z 1-26, A-Z 27-52)" do
       expect(subject.priority).to eq(16)
     end
+  end
+end
+
+describe RucksackGroup do
+  let(:rucksack1) { Rucksack.new("vJrwpWtwJgWrhcsFMMfFFhFp") }
+  let(:rucksack2) { Rucksack.new("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") }
+  let(:rucksack3) { Rucksack.new("PmmdzqPrVvPwwTWBwg") }
+
+  subject { described_class.new([rucksack1, rucksack2, rucksack3]) }
+
+  it "finds the common element across all three rucksacks" do
+    expect(subject.common_element).to eq("r")
+  end
+
+  it "finds the priority for the common element (a-z 1-26, A-Z 27-52)" do
+    expect(subject.priority).to eq(18)
   end
 end
