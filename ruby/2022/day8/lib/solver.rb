718b3be38773c23e7408318438faef97a18b18d1
Cooper Gillan <cooper.gillan@gmail.com>
2022-12-09 21:09:33 +0000

    Little bitta part one cleanup
    Gotta come back to part two later

--- ruby/2022/day8/lib/solver.rb
+++ ruby/2022/day8/lib/solver.rb
@@ -20,110 +20,49 @@ class TopLevelClass
   end
 
   def solve_part1
-    col = row = 0
-    total_trees = 0
-    (0...height).each do |row|
-      (0...width).each do |col|
+    (0...height).map do |row|
+      (0...width).map do |col|
         # Count top and bottom rows, plus first and last columns
-        if tree_visible?(row, col)
-          total_trees += 1
-        end
-      end
-    end
-    total_trees
+        tree_visible?(row, col) ? 1 : 0
+      end.reduce(:+)
+    end.reduce(:+)
   end
 
   def tree_visible?(row, col)
-    return true if viewable_from_left?(row, col)
-    return true if viewable_from_right?(row, col)
-    return true if viewable_from_top?(row, col)
-    return true if viewable_from_bottom?(row, col)
-    false
+    viewable_from_left?(row, col) ||
+      viewable_from_right?(row, col) ||
+      viewable_from_top?(row, col) ||
+      viewable_from_bottom?(row, col)
   end
 
   def viewable_from_left?(row, col)
     return true if col == 0
-    # Each value to left is lower than this value
+
     cols_to_check = (0..(col - 1))
-    puts "cols_to_check: #{cols_to_check}"
-    val_to_check = input_data[row][col]
-    puts "Checking #{val_to_check}"
-    checked = input_data[row][cols_to_check].map do |tree|
-      puts "checking tree: #{tree}"
-      tree.to_i < val_to_check
-    end
-    puts "checked: #{checked}"
-    checked.all?
+    input_data[row][cols_to_check].map do |tree|
+      tree.to_i < input_data[row][col]
+    end.all?
   end
 
   def viewable_from_right?(row, col)
-    puts "col: #{col}"
-    puts "width - 1: #{width - 1}"
-    # return true if col == width - 1
-    # Each value to right is lower than this value
     cols_to_check = ((col + 1)..width)
-    puts "cols_to_check: #{cols_to_check}"
-    val_to_check = input_data[row][col]
-    puts "Checking #{val_to_check}"
     checked = input_data[row][cols_to_check].map do |tree|
-      puts "checking tree: #{tree}"
-      tree.to_i < val_to_check
-    end
-    puts "checked: #{checked}"
-    checked.all?
+      tree.to_i < input_data[row][col]
+    end.all?
   end
 
   def viewable_from_top?(row, col)
-    # Each value to right is lower than this value
     rows_to_check = (0..(row - 1))
-    puts "rows_to_check: #{rows_to_check}"
-    val_to_check = input_data[row][col]
-    puts "Checking #{val_to_check}"
     checked = rows_to_check.map do |check_row|
-      tree = input_data[check_row][col]
-      puts "checking tree: #{tree}"
-      tree.to_i < val_to_check
-    end
-    puts "checked: #{checked}"
-    checked.all?
+      input_data[check_row][col].to_i < input_data[row][col]
+    end.all?
   end
 
   def viewable_from_bottom?(row, col)
-    # Each value to right is lower than this value
-    start_row = row +1
-    # puts "start_row: #{start_row}"
     rows_to_check = ((row + 1)...width)
-    # puts "rows_to_check: #{rows_to_check}"
-    val_to_check = input_data[row][col]
-    # puts "Checking #{val_to_check}"
     checked = rows_to_check.map do |check_row|
-      # puts "check_row: #{check_row}"
-      # require "pry"; binding.pry
-      tree = input_data[check_row][col]
-      # puts "checking tree: #{tree}"
-      tree.to_i < val_to_check
-    end
-    # puts "checked: #{checked}"
-    checked.all?
-  end
-
-  # def solve_part2
-  #   8
-  # end
-end
-
-class ParticularClass
-
-  attr_accessor :input1, :input2
-
-  def initialize(input1, input2)
-    @input1 = input1
-    @input2 = input2
-  end
-
-  def self.from_raw(raw_input)
-    raw_input1, raw_input2 = raw_input
-    new(raw_input1, raw_input2)
+      input_data[check_row][col].to_i < input_data[row][col]
+    end.all?
   end
 end
 
@@ -132,7 +71,4 @@ if $PROGRAM_NAME  == __FILE__
 
   part1 = top_level_instance.solve_part1
   puts "Part one answer: #{part1}"
-
-  # part2 = top_level_instance.solve_part2
-  # puts "Part two answer: #{part2}"
 end
--- ruby/2022/day8/spec/solver_spec.rb
+++ ruby/2022/day8/spec/solver_spec.rb
@@ -95,22 +95,4 @@ describe TopLevelClass do
       expect(subject.solve_part1).to eq(21)
     end
   end
-
-  context "#solve_part2" do
-    it "solves part two" do
-      expect(subject.solve_part2).to eq(8)
-    end
-  end
-end
-
-describe ParticularClass do
-  context "#from_raw" do
-    let(:raw_input) { ["A", "Z"] }
-    subject { described_class.from_raw(raw_input) }
-
-    it "translates the raw input into separate values" do
-      expect(subject.input1).to eq("A")
-      expect(subject.input2).to eq("Z")
-    end
-  end
 end
